75ecb7b0c7f12f4dfb222067be97c6d00582e5b2
Alexis Duburcq <alexis.duburcq@gmail.com>
2025-03-16 16:05:05 -0700

    Add 2-sticks unit test.

--- tests/test_rigid_physics.py
+++ tests/test_rigid_physics.py
@@ -31,10 +31,34 @@ def box_plan():
     return mjcf
 
 
+@pytest.fixture(scope="session")
+def chain_capsule_hinge():
+    mjcf = ET.Element("mujoco", model="two_stick_robot")
+    ET.SubElement(mjcf, "option", timestep="0.01")
+    default = ET.SubElement(mjcf, "default")
+    ET.SubElement(default, "geom", contype="1", conaffinity="1", condim="3")
+    worldbody = ET.SubElement(mjcf, "worldbody")
+    link0 = ET.SubElement(worldbody, "body", name="body1", pos="0.1 0.2 0.0", quat="0.707 0 0.707 0")
+    ET.SubElement(link0, "geom", type="capsule", fromto="0 0 0 0.5 0 0", size="0.05", rgba="1 0 0 0.4")
+    link1 = ET.SubElement(link0, "body", name="body2", pos="0.5 0.2 0.0", quat="0.92388 0 0 0.38268")
+    ET.SubElement(link1, "geom", type="capsule", fromto="0 0 0 0.5 0 0", size="0.05")
+    ET.SubElement(link1, "joint", type="hinge", name="joint1", axis="0 0 1", pos="0.0 0.0 0.0")
+    link2 = ET.SubElement(link1, "body", name="body3", pos="0.5 0.2 0.0", quat="0.92388 0 0.38268 0.0")
+    ET.SubElement(link2, "geom", type="capsule", fromto="0 0 0 0.5 0 0", size="0.05")
+    ET.SubElement(link2, "joint", type="hinge", name="joint2", axis="0 1 0")
+    return mjcf
+
+
 @pytest.mark.parametrize("model_name", ["box_plan"])
-@pytest.mark.parametrize("gs_solver", [gs.constraint_solver.CG])
-@pytest.mark.parametrize("gs_integrator", [gs.integrator.implicitfast])
-@pytest.mark.parametrize("backend", ["cpu"], indirect=True)
+@pytest.mark.parametrize(
+    "gs_solver",
+    [gs.constraint_solver.CG, gs.constraint_solver.Newton],
+)
+@pytest.mark.parametrize(
+    "gs_integrator",
+    [gs.integrator.implicitfast, gs.integrator.Euler],
+)
+@pytest.mark.parametrize("backend", [gs.cpu], indirect=True)
 def test_box_plan_dynamics(gs_sim, mj_sim):
     (gs_robot,) = gs_sim.entities
     cube_pos = np.array([0.0, 0.0, 0.6])
@@ -43,3 +67,21 @@ def test_box_plan_dynamics(gs_sim, mj_sim):
     qpos = np.concatenate((cube_pos, cube_quat))
     qvel = np.zeros((gs_robot.n_dofs,))
     simulate_and_check_mujoco_consistency(gs_sim, mj_sim, qpos, qvel, num_steps=150)
+
+
+@pytest.mark.parametrize("model_name", ["chain_capsule_hinge"])
+@pytest.mark.parametrize(
+    "gs_solver",
+    [gs.constraint_solver.CG, gs.constraint_solver.Newton],
+)
+@pytest.mark.parametrize(
+    "gs_integrator",
+    [gs.integrator.implicitfast, gs.integrator.Euler],
+)
+@pytest.mark.parametrize("backend", [gs.cpu], indirect=True)
+def test_single_joint_1dof_chain(gs_sim, mj_sim):
+    # TODO: Using procedurally generated capsule to avoid
+    (gs_robot,) = gs_sim.entities
+    qpos = np.zeros((gs_robot.n_dofs,))
+    qvel = np.zeros((gs_robot.n_dofs,))
+    simulate_and_check_mujoco_consistency(gs_sim, mj_sim, qpos, qvel, num_steps=500)
