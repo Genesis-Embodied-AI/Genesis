870cf6df51ca6a2f75e52c57ad0126c3d976ed98
Alexis Duburcq <alexis.duburcq@gmail.com>
2025-03-21 22:32:22 -0700

    Add more unit tests.

--- tests/test_rigid_physics.py
+++ tests/test_rigid_physics.py
@@ -107,18 +107,14 @@ def chain_capsule_hinge_capsule(asset_tmp_path):
     "gs_solver",
     [gs.constraint_solver.CG],  # FIXME: , gs.constraint_solver.Newton],
 )
-@pytest.mark.parametrize(
-    "gs_integrator",
-    [gs.integrator.implicitfast, gs.integrator.Euler],
-)
+@pytest.mark.parametrize("gs_integrator", [gs.integrator.implicitfast, gs.integrator.Euler])
 @pytest.mark.parametrize("backend", [gs.cpu], indirect=True)
 def test_box_plan_dynamics(gs_sim, mj_sim):
-    (gs_robot,) = gs_sim.entities
     cube_pos = np.array([0.0, 0.0, 0.6])
     cube_quat = np.random.rand(4)
     cube_quat /= np.linalg.norm(cube_quat)
     qpos = np.concatenate((cube_pos, cube_quat))
-    qvel = np.random.rand(gs_robot.n_dofs) * 0.2
+    qvel = np.random.rand(6) * 0.2
     simulate_and_check_mujoco_consistency(gs_sim, mj_sim, qpos, qvel, num_steps=150)
 
 
@@ -127,32 +123,75 @@ def test_box_plan_dynamics(gs_sim, mj_sim):
 @pytest.mark.parametrize("gs_integrator", [gs.integrator.implicitfast, gs.integrator.Euler])
 @pytest.mark.parametrize("backend", [gs.cpu, gs.gpu], indirect=True)
 def test_box_box_dynamics(gs_sim):
-    for _ in range(50):
+    (gs_robot,) = gs_sim.entities
+    for _ in range(20):
         cube1_pos = np.array([0.0, 0.0, 0.2])
         cube1_quat = np.array([1.0, 0.0, 0.0, 0.0])
-        cube2_pos = np.array([0.0, 0.0, 0.6 + 0.4 * np.random.rand()])
-        cube2_quat = gs.utils.geom.xyz_to_quat(np.array([0.0, 0.0, 2 * np.pi * np.random.rand()]), degrees=False)
+        cube2_pos = np.array([0.0, 0.0, 0.65 + 0.1 * np.random.rand()])
+        cube2_quat = gs.utils.geom.xyz_to_quat(
+            np.array([*(0.15 * np.random.rand(2)), np.pi * np.random.rand()]), degrees=False
+        )
         init_simulators(gs_sim, qpos=np.concatenate((cube1_pos, cube1_quat, cube2_pos, cube2_quat)))
-        for i in range(50):
+        for i in range(100):
             gs_sim.scene.step()
-
-            gs_n_contacts = gs_sim.rigid_solver.collider.n_contacts.to_numpy()[0]
-            if i > 20:
-                gs_n_constraints = gs_sim.rigid_solver.constraint_solver.n_constraints.to_numpy()[0]
-
-                gs_contact_pos = gs_sim.rigid_solver.collider.contact_data.pos.to_numpy()[:gs_n_contacts, 0]
-                # for pos in gs_contact_pos:
-                #     gs_sim.scene.draw_debug_sphere(pos, radius=0.01, color=(1, 0, 0, 1))
-
-                gs_contact_normal = gs_sim.rigid_solver.collider.contact_data.normal.to_numpy()[:gs_n_contacts, 0]
-                gs_penetration = gs_sim.rigid_solver.collider.contact_data.penetration.to_numpy()[:gs_n_contacts, 0]
-
-                # gs_sim.scene.viewer.run()
-                breakpoint()
-
             if gs_sim.scene.visualizer:
                 gs_sim.scene.visualizer.update()
-        breakpoint()
+
+        qvel = gs_robot.get_dofs_velocity().cpu()
+        np.testing.assert_allclose(qvel, 0, atol=1e-2)
+        qpos = gs_robot.get_dofs_position().cpu()
+        np.testing.assert_allclose(qpos[8], 0.6, atol=1e-3)
+
+
+@pytest.mark.parametrize("box_box_detection", [False, True])
+@pytest.mark.parametrize("backend", [gs.gpu, gs.cpu], indirect=True)
+def test_many_boxes_dynamics(box_box_detection, show_viewer):
+    scene = gs.Scene(
+        rigid_options=gs.options.RigidOptions(
+            dt=0.01,
+            box_box_detection=box_box_detection,
+            max_collision_pairs=1000,
+        ),
+        viewer_options=gs.options.ViewerOptions(
+            camera_pos=(10, 10, 10),
+            camera_lookat=(0.0, 0.0, 0.0),
+            camera_fov=40,
+        ),
+        show_viewer=show_viewer,
+    )
+
+    plane = scene.add_entity(
+        gs.morphs.Plane(),
+    )
+    for n in range(5**3):
+        i, j, k = int(n / 25), int(n / 5) % 5, n % 5
+        scene.add_entity(
+            gs.morphs.Box(
+                pos=(i * 1.01, j * 1.01, k * 1.01 + 0.5),
+                size=(1.0, 1.0, 1.0),
+            ),
+            surface=gs.surfaces.Default(
+                color=(*np.random.rand(3), 0.7),
+            ),
+        )
+    scene.build()
+
+    for i in range(150):
+        scene.step()
+
+    for n, entity in enumerate(scene.entities[1:]):
+        i, j, k = int(n / 25), int(n / 5) % 5, n % 5
+        qvel = entity.get_dofs_velocity()
+        np.testing.assert_allclose(qvel, 0, atol=0.02)
+    for n, entity in enumerate(scene.entities[1:]):
+        i, j, k = int(n / 25), int(n / 5) % 5, n % 5
+        qpos = entity.get_dofs_position()
+        qpos0 = np.array((i * 1.01, j * 1.01, k * 1.01 + 0.5))
+        np.testing.assert_allclose(qpos[:3], qpos0, atol=0.05)
+        np.testing.assert_allclose(qpos[3:], 0, atol=0.02)
+
+    if show_viewer:
+        scene.viewer.stop()
 
 
 @pytest.mark.parametrize("model_name", ["chain_capsule_hinge_mesh"])  # FIXME: , "chain_capsule_hinge_capsule"])
@@ -160,10 +199,7 @@ def test_box_box_dynamics(gs_sim):
     "gs_solver",
     [gs.constraint_solver.CG],  # FIXME: , gs.constraint_solver.Newton],
 )
-@pytest.mark.parametrize(
-    "gs_integrator",
-    [gs.integrator.implicitfast, gs.integrator.Euler],
-)
+@pytest.mark.parametrize("gs_integrator", [gs.integrator.implicitfast, gs.integrator.Euler])
 @pytest.mark.parametrize("backend", [gs.cpu], indirect=True)
 def test_simple_kinematic_chain(gs_sim, mj_sim):
     simulate_and_check_mujoco_consistency(gs_sim, mj_sim, num_steps=200)
@@ -201,6 +237,136 @@ def test_robot_kinematics(gs_sim, mj_sim):
     for _ in range(100):
         qpos = dof_bounds[:, 0] + dof_bounds[:, 1] * np.random.rand(gs_robot.n_qs)
         init_simulators(gs_sim, mj_sim, qpos)
+        check_mujoco_data_consistency(gs_sim, mj_sim, atol=(1e-9 if gs.np_float == np.float64 else 1e-6))
+
+
+@pytest.mark.parametrize("backend", [gs.gpu, gs.cpu], indirect=True)
+def test_inverse_kinematics(show_viewer):
+    # create and build the scene
+    scene = gs.Scene(
+        viewer_options=gs.options.ViewerOptions(
+            camera_pos=(3, -1, 1.5),
+            camera_lookat=(0.0, 0.0, 0.5),
+            camera_fov=30,
+            max_FPS=60,
+        ),
+        sim_options=gs.options.SimOptions(
+            dt=0.01,
+        ),
+        rigid_options=gs.options.RigidOptions(
+            box_box_detection=True,
+        ),
+        show_viewer=show_viewer,
+    )
+    plane = scene.add_entity(
+        gs.morphs.Plane(),
+    )
+    cube = scene.add_entity(
+        gs.morphs.Box(
+            size=(0.04, 0.04, 0.04),
+            pos=(0.65, 0.0, 0.02),
+        ),
+        surface=gs.surfaces.Plastic(color=(1, 0, 0)),
+    )
+    cube_2 = scene.add_entity(
+        gs.morphs.Box(
+            size=(0.04, 0.04, 0.04),
+            pos=(0.4, 0.2, 0.02),
+        ),
+        surface=gs.surfaces.Plastic(color=(0, 1, 0)),
+    )
+    franka = scene.add_entity(
+        gs.morphs.MJCF(file="xml/franka_emika_panda/panda.xml"),
+        vis_mode="collision",
+    )
+    scene.build()
+
+    motors_dof = np.arange(7)
+    fingers_dof = np.arange(7, 9)
+    end_effector = franka.get_link("hand")
+
+    # set control gains
+    franka.set_dofs_kp(
+        np.array([4500, 4500, 3500, 3500, 2000, 2000, 2000, 100, 100]),
+    )
+    franka.set_dofs_kv(
+        np.array([450, 450, 350, 350, 200, 200, 200, 10, 10]),
+    )
+    franka.set_dofs_force_range(
+        np.array([-87, -87, -87, -87, -12, -12, -12, -100, -100]),
+        np.array([87, 87, 87, 87, 12, 12, 12, 100, 100]),
+    )
+
+    # move to pre-grasp pose
+    qpos = franka.inverse_kinematics(
+        link=end_effector,
+        pos=np.array([0.65, 0.0, 0.25]),
+        quat=np.array([0, 1, 0, 0]),
+    )
+    # gripper open pos
+    qpos[-2:] = 0.04
+    path = franka.plan_path(
+        qpos_goal=qpos,
+        num_waypoints=100,  # 1s duration
+    )
+    # execute the planned path
+    for waypoint in path:
+        franka.control_dofs_position(waypoint)
+        franka.control_dofs_force(np.array([0.5, 0.5]), fingers_dof)
+        scene.step()
+
+    # allow robot to reach the last waypoint
+    for i in range(100):
+        scene.step()
+
+    # reach
+    qpos = franka.inverse_kinematics(
+        link=end_effector,
+        pos=np.array([0.65, 0.0, 0.130]),
+        quat=np.array([0, 1, 0, 0]),
+    )
+    franka.control_dofs_position(qpos[:-2], motors_dof)
+    for i in range(50):
+        scene.step()
+
+    # grasp
+    franka.control_dofs_position(qpos[:-2], motors_dof)
+    franka.control_dofs_force(np.array([-0.5, -0.5]), fingers_dof)
+    for i in range(50):
+        scene.step()
+
+    # lift
+    qpos = franka.inverse_kinematics(
+        link=end_effector,
+        pos=np.array([0.65, 0.0, 0.28]),
+        quat=np.array([0, 1, 0, 0]),
+    )
+    franka.control_dofs_position(qpos[:-2], motors_dof)
+    for i in range(50):
+        scene.step()
+
+    # reach
+    qpos = franka.inverse_kinematics(
+        link=end_effector,
+        pos=np.array([0.4, 0.2, 0.18]),
+        quat=np.array([0, 1, 0, 0]),
+    )
+    franka.control_dofs_position(qpos[:-2], motors_dof)
+    for i in range(100):
+        scene.step()
+
+    # release
+    franka.control_dofs_position(np.array([0.4, 0.4]), fingers_dof)
+    for i in range(250):
+        scene.step()
+
+    qvel = cube.get_dofs_velocity().cpu()
+    np.testing.assert_allclose(qvel, 0, atol=1e-2)
+    qpos = cube.get_dofs_position().cpu()
+    np.testing.assert_allclose(qpos[2], 0.06, atol=1e-3)
+
+    if show_viewer:
+        scene.viewer.stop()
 
 
 def test_nonconvex_collision(show_viewer):
@@ -223,52 +389,13 @@ def test_nonconvex_collision(show_viewer):
         ),
     )
     scene.build()
+
     ball.set_dofs_velocity(np.random.rand(ball.n_dofs) * 0.8)
     for i in range(1500):
         scene.step()
+
     qvel = scene.sim.rigid_solver.dofs_state.vel.to_numpy()[:, 0]
     np.testing.assert_allclose(qvel, 0, atol=0.1)
 
-
-def test_stacked_boxes(show_viewer):
-    scene = gs.Scene(
-        rigid_options=gs.options.RigidOptions(
-            dt=0.01,
-            max_collision_pairs=250,
-        ),
-        viewer_options=gs.options.ViewerOptions(
-            camera_pos=(10, 10, 10),
-            camera_lookat=(0.0, 0.0, 0.0),
-            camera_fov=40,
-        ),
-        show_viewer=show_viewer,
-    )
-    plane = scene.add_entity(
-        gs.morphs.Plane(),
-    )
-    for n in range(5**3):
-        i, j, k = int(n / 25), int(n / 5) % 5, n % 5
-        scene.add_entity(
-            gs.morphs.Box(
-                pos=(i * 1.01, j * 1.01, k * 1.01 + 0.5),
-                size=(1.0, 1.0, 1.0),
-            ),
-            surface=gs.surfaces.Default(
-                color=(*np.random.rand(3), 0.7),
-            ),
-        )
-
-    scene.build()
-    for i in range(150):
-        scene.step()
-
-    for n, entity in enumerate(scene.entities[1:]):
-        i, j, k = int(n / 25), int(n / 5) % 5, n % 5
-        qvel = entity.get_dofs_velocity()
-        np.testing.assert_allclose(qvel, 0, atol=0.01)
-    for n, entity in enumerate(scene.entities[1:]):
-        i, j, k = int(n / 25), int(n / 5) % 5, n % 5
-        qpos = entity.get_dofs_position()
-        qpos0 = np.array((i * 1.01, j * 1.01, k * 1.01 + 0.5))
-        np.testing.assert_allclose(qpos[:3], qpos0, atol=0.05)
-        np.testing.assert_allclose(qpos[3:], 0, atol=0.02)
+    if show_viewer:
+        scene.viewer.stop()
