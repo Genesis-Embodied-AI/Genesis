8f3060ec2ad1930f32e78357de3bc59aa660eef4
Alexis DUBURCQ <alexis.duburcq@gmail.com>
2025-03-24 15:41:58 -0700

    Fix broken libompl dyn lib import logics. (#923)

--- genesis/__init__.py
+++ genesis/__init__.py
@@ -1,10 +1,12 @@
 # import taichi while suppressing its output
 import os
 import sys
+import site
 import ctypes
 import atexit
 import logging as _logging
 import traceback
+from platform import system
 from unittest.mock import patch
 
 _ti_outputs = []
@@ -31,6 +33,50 @@ _initialized = False
 backend = None
 exit_callbacks = []
 global_scene_list = set()
+
+
+############ fix python DLL search dir for OMPL ############
+try:
+    import ompl
+
+    _is_ompl_available = True
+except ImportError:
+    _is_ompl_available = False
+
+
+def dll_loader(lib, path):
+    # First, try the user-specified path
+    sys = system()
+    if sys == "Windows":
+        ext = ".dll"
+    elif sys == "Darwin":
+        ext = ".dylib"
+    else:  # Linux, other UNIX systems
+        ext = ".so"
+    fname = f"lib{lib}{ext}"
+    fpath = os.path.join(path, fname)
+
+    # Fallback to site-packages
+    if not os.path.isfile(fpath):
+        for sitepackagedir in site.getsitepackages():
+            for _fname in os.listdir(sitepackagedir):
+                _fpath = os.path.join(sitepackagedir, _fname)
+                if os.path.isfile(_fpath):
+                    if _fname.startswith(fname):
+                        fpath = _fpath
+                        break
+
+    # Fallback to system loading and pray
+    if not os.path.isfile(fpath):
+        fpath = find_library(lib)
+
+    cdll = ctypes.CDLL(fpath, ctypes.RTLD_GLOBAL)
+    if cdll is None:
+        gs.raise_exception(f"Failed to load dynamic library '{lib}' (search path '{path}').")
+
+
+if _is_ompl_available:
+    ompl.dll_loader = dll_loader
 
 
 ########################## init ##########################
