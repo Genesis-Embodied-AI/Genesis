[BUG FIX] Improve numerical stability of collision detection (#966)
* Apply morph pose as transform wrt original base pose
* Randomize collision mesh colors
* More challenging convex decomposition example / unit test
* Enable decimation by default if convexify is enabled. 
* Do not decimate before convex decomposition.
* Reduce MPR epsilon threshold (basically disable it for float32) to avoid jerky contact points 
* Enforce option constraint time constant systematically if and if specified.

genesis/engine/entities/rigid_entity/rigid_entity.py
@@ -343,13 +343,21 @@ class RigidEntity(Entity):
             if l_info["parent_idx"] < 0:
                 for j_info in link_j_infos:
                     if j_info["type"] != gs.JOINT_TYPE.FIXED:  # base link
-                        if morph.pos is not None:
-                            l_info["pos"] = np.array(morph.pos)
-                            gs.logger.warning("Overriding base link's pos with user provided value in morph.")
-                        if morph.quat is not None:
-                            l_info["quat"] = np.array(morph.quat)
-                            gs.logger.warning("Overriding base link's quat with user provided value in morph.")
-
+                        if morph.pos is not None or morph.quat is not None:
+                            gs.logger.info("Applying offset to base link's pose with user provided value in morph.")
+                            pos = np.asarray(l_info.get("pos", (0.0, 0.0, 0.0)))
+                            quat = np.asarray(l_info.get("quat", (1.0, 0.0, 0.0, 0.0)))
+                            if morph.pos is None:
+                                pos_offset = np.zeros((3,))
+                            else:
+                                pos_offset = np.asarray(morph.pos)
+                            if morph.quat is None:
+                                quat_offset = np.array((1.0, 0.0, 0.0, 0.0))
+                            else:
+                                quat_offset = np.asarray(morph.quat)
+                            l_info["pos"], l_info["quat"] = gu.transform_pos_quat_by_trans_quat(
+                                pos, quat, pos_offset, quat_offset
+                            )
                     if j_info["type"] == gs.JOINT_TYPE.FREE:
                         # in this case, l_info['pos'] and l_info['quat'] are actually not used in solver,
                         # but this initial value will be reflected

genesis/engine/solvers/rigid/rigid_solver_decomp.py
@@ -23,13 +23,21 @@ IMP_MIN = 0.0001
 # maximum constraint impedance
 IMP_MAX = 0.9999
 
+# Minimum ratio between simulation timestep `_substep_dt` and time constant of constraints
+TIME_CONSTANT_SAFETY_FACTOR = 2.0
 
-def _sanitize_sol_params(sol_params, substep_dt, default_resolve_time=None):
+
+def _sanitize_sol_params(sol_params, min_resolve_time, force_resolve_time=None):
     timeconst, dampratio, dmin, dmax, width, mid, power = sol_params.T
-    if default_resolve_time is not None:
-        # Use default default resolve time if and only if solref is not set
-        timeconst[timeconst == 0.0] = default_resolve_time
-    timeconst = np.maximum(timeconst, 2 * substep_dt)
+    if force_resolve_time is not None:
+        timeconst = np.full_like(timeconst, force_resolve_time)
+    min_timeconst = np.min(timeconst)
+    if (min_timeconst + 1e-6 < min_resolve_time).any():
+        gs.logger.warning(
+            f"Constraint solver time constant was increased to avoid numerical instability (from `{min_timeconst:0.6g}` "
+            f"to `{min_resolve_time:0.6g}`). Decrease simulation timestep to avoid altering the original value."
+        )
+    timeconst = np.maximum(timeconst, min_resolve_time)
     dmin = np.clip(dmin, IMP_MIN, IMP_MAX)
     dmax = np.clip(dmax, IMP_MIN, IMP_MAX)
     mid = np.clip(mid, IMP_MIN, IMP_MAX)
@@ -67,10 +75,8 @@ class RigidSolver(Solver):
         self._hibernation_thresh_vel = options.hibernation_thresh_vel
         self._hibernation_thresh_acc = options.hibernation_thresh_acc
 
-        if options.constraint_resolve_time is None:
-            self._sol_constraint_resolve_time = 2 * self._substep_dt
-        else:
-            self._sol_constraint_resolve_time = options.constraint_resolve_time
+        self._sol_constraint_min_resolve_time = TIME_CONSTANT_SAFETY_FACTOR * self._substep_dt
+        self._sol_constraint_resolve_time = options.constraint_resolve_time
 
         if options.contact_resolve_time is not None:
             self._sol_contact_resolve_time = options.contact_resolve_time
@@ -372,7 +378,9 @@ class RigidSolver(Solver):
         if is_nonempty:  # handle the case where there is a link with no dofs -- otherwise may cause invalid memory
             # Make sure that the constraints parameters are valid
             dofs_sol_params = np.concatenate([joint.dofs_sol_params for joint in joints], dtype=gs.np_float)
-            dofs_sol_params = _sanitize_sol_params(dofs_sol_params, self._substep_dt, self._sol_constraint_resolve_time)
+            dofs_sol_params = _sanitize_sol_params(
+                dofs_sol_params, self._sol_constraint_min_resolve_time, self._sol_constraint_resolve_time
+            )
 
             self._kernel_init_dof_fields(
                 dofs_motion_ang=np.concatenate([joint.dofs_motion_ang for joint in joints], dtype=gs.np_float),
@@ -562,7 +570,9 @@ class RigidSolver(Solver):
         # Make sure that the constraints parameters are valid
         joints = tuple(chain.from_iterable(self.joints))
         joints_sol_params = np.concatenate([joint.sol_params for joint in joints], dtype=gs.np_float)
-        joints_sol_params = _sanitize_sol_params(joints_sol_params, self._substep_dt)
+        joints_sol_params = _sanitize_sol_params(
+            joints_sol_params, self._sol_constraint_min_resolve_time, self._sol_constraint_resolve_time
+        )
 
         self._kernel_init_joint_fields(
             joints_type=np.array([joint.type for joint in joints], dtype=gs.np_int),
@@ -881,7 +891,7 @@ class RigidSolver(Solver):
             geoms = self.geoms
             geoms_sol_params = np.array([geom.sol_params for geom in geoms], dtype=gs.np_float)
             geoms_sol_params = _sanitize_sol_params(
-                geoms_sol_params, self._substep_dt, self._sol_constraint_resolve_time
+                geoms_sol_params, self._sol_constraint_min_resolve_time, self._sol_constraint_resolve_time
             )
 
             self._kernel_init_geom_fields(
@@ -1190,7 +1200,9 @@ class RigidSolver(Solver):
 
             equalities_sol_params = np.array([equality.sol_params for equality in equalities], dtype=gs.np_float)
             equalities_sol_params = _sanitize_sol_params(
-                equalities_sol_params, self._substep_dt, self._sol_constraint_resolve_time
+                equalities_sol_params,
+                self._sol_constraint_min_resolve_time,
+                self._sol_constraint_resolve_time,
             )
 
             self._kernel_init_equality_fields(
@@ -3977,7 +3989,9 @@ class RigidSolver(Solver):
         assert len(sol_params) == 7
 
         # Make sure that the constraints parameters are valid
-        sol_params = _sanitize_sol_params(sol_params)
+        sol_params = _sanitize_sol_params(
+            sol_params, self._sol_constraint_min_resolve_time, self._sol_constraint_resolve_time
+        )
 
         self._kernel_set_global_sol_params(sol_params)
 

genesis/utils/mesh.py
@@ -4,14 +4,15 @@ import pickle as pkl
 from io import BytesIO
 from urllib import request
 
+import numpy as np
+import trimesh
+from PIL import Image
+
 import coacd
 import igl
-import numpy as np
 import pygltflib
 import pyvista as pv
 import tetgen
-import trimesh
-from PIL import Image
 
 import genesis as gs
 from genesis.ext import fast_simplification
@@ -171,14 +172,7 @@ def surface_uvs_to_trimesh_visual(surface, uvs=None, n_verts=None):
     return visual
 
 
-def convex_decompose(mesh, decimate, decimate_face_num, coacd_options):
-    if decimate:
-        if mesh.vertices.shape[0] > 3:
-            if len(mesh.faces) > decimate_face_num:
-                mesh = trimesh.Trimesh(
-                    *fast_simplification.simplify(mesh.vertices, mesh.faces, target_count=decimate_face_num, agg=0)
-                )
-
+def convex_decompose(mesh, coacd_options):
     # compute file name via hashing for caching
     cvx_path = get_cvx_path(mesh.vertices, mesh.faces, coacd_options)
 
@@ -300,7 +294,7 @@ def postprocess_collision_geoms(
                 cmesh = trimesh.convex.convex_hull(tmesh)
                 volume_err = cmesh.volume / tmesh.volume - 1.0
             if volume_err > decompose_error_threshold:
-                tmeshes = convex_decompose(tmesh, decimate, decimate_face_num, coacd_options)
+                tmeshes = convex_decompose(tmesh, coacd_options)
                 meshes = [gs.Mesh.from_trimesh(tmesh, surface=gs.surfaces.Collision()) for tmesh in tmeshes]
                 _g_infos += [{**g_info, **dict(mesh=mesh)} for mesh in meshes]
             else:
@@ -313,11 +307,14 @@ def postprocess_collision_geoms(
         mesh = g_info["mesh"]
         tmesh = mesh.trimesh
         num_vertices = len(tmesh.vertices)
-        if not convexify and not decimate and num_vertices > 5000:
+        if not decimate and num_vertices > 5000:
             gs.logger.warning(
                 f"At least one of the meshes contain many vertices ({num_vertices}). Consider setting "
-                "'morph.decimate=True' or 'morph.convexify=True' to speed up collision detection and improve numerical "
-                "stability."
+                "'morph.decimate=True' to speed up collision detection and improve numerical stability."
+            )
+        if decimate and decimate_face_num < 100:
+            gs.logger.warning(
+                "`decimate_face_num` should be greater than 100 to ensure sufficient geometry details are preserved."
             )
         mesh = gs.Mesh.from_trimesh(
             mesh=tmesh,
@@ -326,6 +323,8 @@ def postprocess_collision_geoms(
             decimate_face_num=decimate_face_num,
             surface=gs.surfaces.Collision(),
         )
+        # Randomize collision mesh colors. The is especially useful to check convex decomposition.
+        mesh.set_color((*np.random.rand(3), 1.0))
         _g_infos.append({**g_info, **dict(mesh=mesh)})
 
     return _g_infos

tests/test_rigid_physics.py
@@ -238,7 +238,7 @@ def test_box_box_dynamics(gs_sim):
         qvel = gs_robot.get_dofs_velocity().cpu()
         np.testing.assert_allclose(qvel, 0, atol=1e-2)
         qpos = gs_robot.get_dofs_position().cpu()
-        np.testing.assert_allclose(qpos[8], 0.6, atol=1e-3)
+        np.testing.assert_allclose(qpos[8], 0.6, atol=2e-3)
 
 
 @pytest.mark.parametrize("box_box_detection, dynamics", [(False, False), (False, True), (True, False)])
@@ -315,6 +315,8 @@ def test_simple_kinematic_chain(gs_sim, mj_sim, atol):
 @pytest.mark.parametrize("gs_integrator", [gs.integrator.Euler])
 @pytest.mark.parametrize("backend", [gs.cpu])
 def test_walker(gs_sim, mj_sim, atol):
+    # Force numpy seed because this test is very sensitive to the initial condition
+    np.random.seed(0)
     (gs_robot,) = gs_sim.entities
     qpos = np.zeros((gs_robot.n_qs,))
     qpos[2] += 0.5
@@ -359,12 +361,12 @@ def test_stickman(gs_sim, mj_sim, atol):
     init_simulators(gs_sim)
 
     # Run the simulation for a few steps
-    for i in range(6000):
+    for i in range(2000):
         gs_sim.scene.step()
 
     (gs_robot,) = gs_sim.entities
     qvel = gs_robot.get_dofs_velocity().cpu()
-    np.testing.assert_allclose(qvel, 0, atol=0.1)
+    np.testing.assert_allclose(qvel, 0, atol=0.3)
     qpos = gs_robot.get_dofs_position().cpu()
     assert np.linalg.norm(qpos[:2]) < 1.3
     body_z = gs_sim.rigid_solver.links_state.pos.to_numpy()[:-1, 0, 2]
@@ -498,7 +500,7 @@ def move_cube(use_suction, show_viewer):
     qvel = cube.get_dofs_velocity().cpu()
     np.testing.assert_allclose(qvel, 0, atol=0.05)
     qpos = cube.get_dofs_position().cpu()
-    np.testing.assert_allclose(qpos[2], 0.06, atol=1e-3)
+    np.testing.assert_allclose(qpos[2], 0.06, atol=2e-3)
 
     if show_viewer:
         scene.viewer.stop()
@@ -516,6 +518,7 @@ def test_suction_cup(show_viewer):
     move_cube(use_suction, show_viewer)
 
 
+@pytest.mark.parametrize("backend", [gs.cpu])
 def test_nonconvex_collision(show_viewer):
     scene = gs.Scene(show_viewer=show_viewer, show_FPS=False)
     tank = scene.add_entity(
@@ -543,7 +546,7 @@ def test_nonconvex_collision(show_viewer):
         scene.step()
 
     qvel = scene.sim.rigid_solver.dofs_state.vel.to_numpy()[:, 0]
-    np.testing.assert_allclose(qvel, 0, atol=0.1)
+    np.testing.assert_allclose(qvel, 0, atol=0.15)
 
     if show_viewer:
         scene.viewer.stop()
@@ -556,7 +559,7 @@ def test_convexify(show_viewer):
     # Then run a simulation to see if it explodes, i.e. objects are at reset inside tank.
     scene = gs.Scene(
         rigid_options=gs.options.RigidOptions(
-            dt=0.005,
+            dt=0.004,
         ),
         show_viewer=show_viewer,
         show_FPS=False,
@@ -566,7 +569,7 @@ def test_convexify(show_viewer):
             file="meshes/tank.obj",
             scale=5.0,
             fixed=True,
-            euler=(90, 0, 90),
+            euler=(75, 15, 90),
         ),
         vis_mode="collision",
     )
@@ -591,6 +594,7 @@ def test_convexify(show_viewer):
 
     # Make sure that all the geometries in the scene are convex
     assert gs_sim.rigid_solver.geoms_info.is_convex.to_numpy().all()
+    assert not gs_sim.rigid_solver.collider._has_nonconvex_nonterrain
 
     # There should be only one geometry for the apple as it can be convexify without decomposition,
     # but for the others it is hard to tell... Let's use some reasonable guess.
@@ -600,14 +604,14 @@ def test_convexify(show_viewer):
     assert 5 <= len(cup.geoms) <= 20
     assert 5 <= len(mug.geoms) <= 40
 
-    for i in range(6000):
+    for i in range(1500):
         scene.step()
 
     for obj in objs:
         qvel = obj.get_dofs_velocity().cpu()
-        np.testing.assert_allclose(qvel, 0, atol=0.5)
+        np.testing.assert_allclose(qvel, 0, atol=0.05)
         qpos = obj.get_dofs_position().cpu()
-        np.testing.assert_array_less(0, qpos[2])
+        np.testing.assert_array_less(-0.1, qpos[2])
         np.testing.assert_array_less(qpos[2], 0.15)
         np.testing.assert_array_less(torch.linalg.norm(qpos[:2]), 0.5)
 
@@ -659,7 +663,7 @@ def test_terrain_generation(show_viewer):
     height_balls = ball.get_pos().cpu()[:, 2]
     height_balls_min = height_balls.min() - 0.1
     height_balls_max = height_balls.max() - 0.1
-    np.testing.assert_allclose(height_balls_min, height_field_min, atol=1e-3)
+    np.testing.assert_allclose(height_balls_min, height_field_min, atol=2e-3)
     assert height_balls_max - height_balls_min > 0.5 * (height_field_max - height_field_min)
 
 