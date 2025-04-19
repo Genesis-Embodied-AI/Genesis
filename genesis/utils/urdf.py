[BUG FIX] Fix robot scaling. (#1039)
* Fix robot scaling.
* More robust genesis destroy.

genesis/utils/urdf.py
@@ -241,8 +241,8 @@ def parse_urdf(morph, surface):
             j_info["dofs_limit"] = np.array(
                 [
                     [
-                        joint.limit.lower if joint.limit.lower is not None else -np.inf,
-                        joint.limit.upper if joint.limit.upper is not None else np.inf,
+                        scale * joint.limit.lower if joint.limit.lower is not None else -np.inf,
+                        scale * joint.limit.upper if joint.limit.upper is not None else np.inf,
                     ]
                 ]
             )