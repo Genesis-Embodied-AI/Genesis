d04aa395ca066f2c778bd234a5b8b7802d985b80
Ziheng Liu <lzh2199@gmail.com>
2025-06-03 18:22:30 -0700

    bvh construction

--- genesis/engine/bvh.py
+++ genesis/engine/bvh.py
@@ -16,6 +16,13 @@ class LBVH(RBC):
     It is used to accelerate collision detection and ray tracing. Linear BVH is a simple BVH that is used to accelerate collision detection and ray tracing using parallelization.
     """
 
+    @ti.dataclass
+    class Node:
+        left: ti.i32  # Index of the left child
+        right: ti.i32  # Index of the right child
+        parent: ti.i32  # Index of the parent node
+        bound: AABB  # Bounding box of the node
+
     def __init__(self, n_aabbs, n_batches):
         self.n_aabbs = n_aabbs
         self.n_batches = n_batches
@@ -23,10 +30,32 @@ class LBVH(RBC):
         self.aabb_min = ti.field(gs.ti_vec3, shape=(n_batches))
         self.aabb_max = ti.field(gs.ti_vec3, shape=(n_batches))
         self.scale = ti.field(gs.ti_vec3, shape=(n_batches))
-        self.morton_codes = ti.field(gs.ti_u32, shape=(n_aabbs, n_batches))
+        self.morton_codes = ti.field(ti.u64, shape=(n_aabbs, n_batches))
+
+        self.hist = ti.field(ti.u32, shape=(256, n_batches))  # Histogram for radix sort
+        self.prefix_sum = ti.field(ti.u32, shape=(256, n_batches))  # Prefix sum for histogram
+        self.offset = ti.field(ti.u32, shape=(n_aabbs, n_batches))  # Offset for radix sort
+        self.tmp_morton_codes = ti.field(ti.u64, shape=(n_aabbs, n_batches))  # Temporary storage for radix sort
+
+        self.nodes = self.Node.field(
+            shape=(n_aabbs * 2 - 1, n_batches)
+        )  # Nodes of the BVH, first n_aabbs - 1 are internal nodes, last n_aabbs are leaf nodes
+        self.internal_node_visited = ti.field(
+            ti.u8, shape=(n_aabbs - 1, n_batches)
+        )  # If an internal node has been visited during traversal
 
     @ti.kernel
-    def build(self, aabbs):
+    def build(self, aabbs: ti.template()):
+        """
+        Build the BVH from the given axis-aligned bounding boxes (AABBs).
+        The AABBs are expected to be in the format of a 2D array with shape (n_aabbs, n_batches),
+        where n_aabbs is the number of AABBs and n_batches is the number of batches.
+        Each AABB is represented by its minimum and maximum corners.
+
+        Notes
+        ------
+        https://research.nvidia.com/sites/default/files/pubs/2012-06_Maximizing-Parallelism-in/karras2012hpg_paper.pdf
+        """
         assert aabbs.shape[0] == self.n_aabbs
         assert aabbs.shape[1] == self.n_batches
 
@@ -46,7 +75,141 @@ class LBVH(RBC):
             for i in ti.static(range(3)):
                 self.scale[i_b][i] = ti.select(scale[i] > 1e-7, 1.0 / scale[i], 1)
 
-    @ti.kernel
-    def test(self):
+        self.compute_morton_codes()
+        self.radix_sort_morton_codes()
+        self.build_radix_tree()
+        self.compute_bounds(aabbs)
+
+    @ti.func
+    def compute_morton_codes(self):
+        for i_a, i_b in ti.ndrange(self.n_aabbs, self.n_batches):
+            center = self.aabb_centers[i_a, i_b] - self.aabb_min[i_b]
+            scaled_center = center * self.scale[i_b]
+            morton_code_x = ti.floor(scaled_center[0] * 1024.0, dtype=ti.u32)
+            morton_code_y = ti.floor(scaled_center[1] * 1024.0, dtype=ti.u32)
+            morton_code_z = ti.floor(scaled_center[2] * 1024.0, dtype=ti.u32)
+            morton_code_x = self.expand_bits(morton_code_x)
+            morton_code_y = self.expand_bits(morton_code_y)
+            morton_code_z = self.expand_bits(morton_code_z)
+            morton_code = (morton_code_x << 2) | (morton_code_y << 1) | (morton_code_z)
+            self.morton_codes[i_a, i_b] = (ti.u64(morton_code) << 32) | ti.u64(i_a)
+
+    # Expands a 10-bit integer into 30 bits by inserting 2 zeros after each bit.
+    @ti.func
+    def expand_bits(self, v):
+        v = (v * ti.u32(0x00010001)) & ti.u32(0xFF0000FF)
+        v = (v * ti.u32(0x00000101)) & ti.u32(0x0F00F00F)
+        v = (v * ti.u32(0x00000011)) & ti.u32(0xC30C30C3)
+        v = (v * ti.u32(0x00000005)) & ti.u32(0x49249249)
+        return v
+
+    # radix sort the morton codes, using 8 bits at a time
+    @ti.func
+    def radix_sort_morton_codes(self):
+        for i in ti.static(range(8)):
+            # Clear histogram
+            for j, i_b in ti.ndrange(256, self.n_batches):
+                self.hist[j, i_b] = 0
+            # Fill histogram
+            for i_a, i_b in ti.ndrange(self.n_aabbs, self.n_batches):
+                code = (self.morton_codes[i_a, i_b] >> (i * 8)) & 0xFF
+                self.offset[i_a, i_b] = ti.atomic_add(self.hist[ti.i32(code), i_b], 1)
+            # Compute prefix sum
+            for i_b in ti.ndrange(self.n_batches):
+                self.prefix_sum[0, i_b] = 0
+                for j in range(1, 256):
+                    self.prefix_sum[j, i_b] = self.prefix_sum[j - 1, i_b] + self.hist[j - 1, i_b]
+            # Reorder morton codes
+            for i_a, i_b in ti.ndrange(self.n_aabbs, self.n_batches):
+                code = (self.morton_codes[i_a, i_b] >> (i * 8)) & 0xFF
+                idx = ti.i32(self.offset[i_a, i_b] + self.prefix_sum[ti.i32(code), i_b])
+                self.tmp_morton_codes[idx, i_b] = self.morton_codes[i_a, i_b]
+
+            # Swap the temporary and original morton codes
+            for i_a, i_b in ti.ndrange(self.n_aabbs, self.n_batches):
+                self.morton_codes[i_a, i_b] = self.tmp_morton_codes[i_a, i_b]
+
+    @ti.func
+    def build_radix_tree(self):
+        # Initialize the first node
         for i_b in ti.ndrange(self.n_batches):
-            print(self.scale[i_b])
+            self.nodes[0, i_b].parent = -1
+
+        # Initialize the leaf nodes
+        for i, i_b in ti.ndrange(self.n_aabbs, self.n_batches):
+            self.nodes[i + self.n_aabbs - 1, i_b].left = -1
+            self.nodes[i + self.n_aabbs - 1, i_b].right = -1
+
+        for i, i_b in ti.ndrange(self.n_aabbs - 1, self.n_batches):
+            d = ti.select(
+                self.delta(i, i + 1, i_b) > self.delta(i, i - 1, i_b),
+                1,
+                -1,
+            )
+
+            delta_min = self.delta(i, i - d, i_b)
+            l_max = ti.u32(2)
+            while self.delta(i, i + l_max * d, i_b) > delta_min:
+                l_max *= 2
+            l = ti.u32(0)
+
+            t = l_max // 2
+            while t > 0:
+                if self.delta(i, i + (l + t) * d, i_b) > delta_min:
+                    l += t
+                t //= 2
+            j = i + l * d
+            delta_node = self.delta(i, j, i_b)
+            s = ti.u32(0)
+            t = (l + 1) // 2
+            while t > 0:
+                if self.delta(i, i + (s + t) * d, i_b) > delta_node:
+                    s += t
+                t = ti.select(t > 1, (t + 1) // 2, 0)
+
+            gamma = i + ti.i32(s) * d + ti.min(d, 0)
+            left = ti.select(ti.min(i, j) == gamma, gamma + self.n_aabbs - 1, gamma)
+            right = ti.select(ti.max(i, j) == gamma + 1, gamma + self.n_aabbs, gamma + 1)
+            self.nodes[i, i_b].left = ti.i32(left)
+            self.nodes[i, i_b].right = ti.i32(right)
+            self.nodes[ti.i32(left), i_b].parent = i
+            self.nodes[ti.i32(right), i_b].parent = i
+
+    @ti.func
+    def delta(self, i, j, i_b):
+        """
+        Compute the longest common prefix (LCP) of the morton codes of two AABBs.
+        """
+        result = -1
+        if j >= 0 and j < self.n_aabbs:
+            result = 64
+            x = self.morton_codes[ti.i32(i), i_b] ^ self.morton_codes[ti.i32(j), i_b]
+            for b in range(64):
+                if x & (ti.u64(1) << (63 - b)):
+                    result = b
+                    break
+        return result
+
+    @ti.func
+    def compute_bounds(self, aabbs: ti.template()):
+        """
+        Compute the bounds of the BVH nodes. Starts from the leaf nodes and works upwards.
+        """
+        for i, i_b in ti.ndrange(self.n_aabbs - 1, self.n_batches):
+            self.internal_node_visited[i, i_b] = ti.u8(0)
+
+        for i, i_b in ti.ndrange(self.n_aabbs, self.n_batches):
+            idx = ti.i32(self.morton_codes[i, i_b])
+            self.nodes[i + self.n_aabbs - 1, i_b].bound.min = aabbs[idx, i_b].min
+            self.nodes[i + self.n_aabbs - 1, i_b].bound.max = aabbs[idx, i_b].max
+
+            cur_idx = self.nodes[i + self.n_aabbs - 1, i_b].parent
+            while cur_idx != -1:
+                visited = ti.u1(ti.atomic_or(self.internal_node_visited[cur_idx, i_b], ti.u8(1)))
+                if not visited:
+                    break
+                left_bound = self.nodes[self.nodes[cur_idx, i_b].left, i_b].bound
+                right_bound = self.nodes[self.nodes[cur_idx, i_b].right, i_b].bound
+                self.nodes[cur_idx, i_b].bound.min = ti.min(left_bound.min, right_bound.min)
+                self.nodes[cur_idx, i_b].bound.max = ti.max(left_bound.max, right_bound.max)
+                cur_idx = self.nodes[cur_idx, i_b].parent
--- tests/test_bvh.py
+++ tests/test_bvh.py
@@ -1,11 +1,115 @@
+import taichi as ti
+import torch
+import numpy as np
 import genesis as gs
+
+
 import pytest
+from .utils import assert_allclose
 
-from genesis.engine.bvh import AABB, LBVH
+
+@pytest.fixture(scope="function")
+def lbvh():
+    """Fixture for a LBVH tree"""
+    from genesis.engine.bvh import LBVH, AABB
+
+    n_aabbs = 10
+    n_batches = 1
+    tree = LBVH(n_aabbs=n_aabbs, n_batches=n_batches)
+    min = np.random.rand(n_aabbs, n_batches, 3).astype(np.float32)
+    max = min + np.random.rand(n_aabbs, n_batches, 3).astype(np.float32)
+    aabbs = AABB.field(
+        shape=(n_aabbs, n_batches),
+        needs_grad=False,
+        layout=ti.Layout.SOA,
+    )
+    aabbs.min.from_numpy(min)
+    aabbs.max.from_numpy(max)
+    tree.build(aabbs)
+    return tree
 
 
-def test_aabb():
-    aabb = AABB(min=gs.ti_vec3(0, 0, 0), max=gs.ti_vec3(1, 1, 1))
+def test_morton_code(lbvh):
+    morton_codes = lbvh.morton_codes.to_numpy()
+    # Check that the morton codes are sorted
+    for i in range(1, morton_codes.shape[0]):
+        assert (
+            morton_codes[i] > morton_codes[i - 1]
+        ), f"Morton codes are not sorted: {morton_codes[i]} < {morton_codes[i - 1]}"
+
+
+@ti.kernel
+def expand_bits(lbvh: ti.template(), x: ti.template(), expanded_x: ti.template()):
+    n_x = x.shape[0]
+    for i in range(n_x):
+        expanded_x[i] = lbvh.expand_bits(x[i])
+
+
+def test_expand_bits():
+    # random integer
+    x_np = np.random.randint(0, 1024, (10,), dtype=np.uint32)
+    x_ti = ti.field(ti.uint32, shape=x_np.shape)
+    x_ti.from_numpy(x_np)
+    expanded_x_ti = ti.field(ti.uint32, shape=x_np.shape)
+    # expand bits
+    from genesis.engine.bvh import LBVH
+
+    lbvh = LBVH(n_aabbs=10, n_batches=1)
+    expand_bits(lbvh, x_ti, expanded_x_ti)
+    expanded_x_np = expanded_x_ti.to_numpy()
+    for i in range(x_np.shape[0]):
+        str_x = f"{x_np[i]:010b}"
+        str_expanded_x = f"{expanded_x_np[i]:030b}"
+        # check that the expanded bits are correct
+        assert str_expanded_x == "".join(
+            f"00{bit}" for bit in str_x
+        ), f"Expected {str_expanded_x}, got {''.join(f'00{bit}' for bit in str_x)}"
+
+
+def test_tree_nodes(lbvh):
+    nodes = lbvh.nodes.to_numpy()
+    n_aabbs = lbvh.n_aabbs
+    n_batches = lbvh.n_batches
+
+    # Check parent-child relationships
+    for i in range(n_aabbs * 2 - 1):
+        for j in range(n_batches):
+            parent = nodes["parent"][i, j]
+            left = nodes["left"][i, j]
+            right = nodes["right"][i, j]
+
+            # Check that the parent is correct
+            if i == 0:  # root node has no parent
+                assert parent == -1
+
+            else:
+                assert (
+                    nodes["left"][parent, j] == i or nodes["right"][parent, j] == i
+                ), f"Node {i} in batch {j} has incorrect parent: {parent}"
+
+            # Check that left and right children are correct
+            if left != -1:
+                assert (
+                    nodes["parent"][left, j] == i
+                ), f"Left child {left} of node {i} in batch {j} has incorrect parent: {nodes['parent'][left, j]}, expected {i}"
+            if right != -1:
+                assert (
+                    nodes["parent"][right, j] == i
+                ), f"Right child {right} of node {i} in batch {j} has incorrect parent: {nodes['parent'][right, j]}, expected {i}"
+
+            if left != -1 and right != -1:
+                # Check that the AABBs of the children are within the AABB of the parent
+                parent_min = nodes["bound"]["min"][i, j]
+                parent_max = nodes["bound"]["max"][i, j]
+                left_min = nodes["bound"]["min"][left, j]
+                left_max = nodes["bound"]["max"][left, j]
+                right_min = nodes["bound"]["min"][right, j]
+                right_max = nodes["bound"]["max"][right, j]
+
+                parent_min_expected = np.minimum(left_min, right_min)
+                parent_max_expected = np.maximum(left_max, right_max)
+                assert_allclose(parent_min, parent_min_expected, rtol=1e-5, atol=1e-5)
+                assert_allclose(parent_max, parent_max_expected, rtol=1e-5, atol=1e-5)
 
 
 if __name__ == "__main__":
