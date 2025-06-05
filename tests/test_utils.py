2f98dac10d03090d5820687f3de110d4ab91ed27
Kashu7100 <echo.six0566@gmail.com>
2025-06-05 14:05:01 -0700

    transform_by_T torch and numpy batched version

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -42,3 +42,91 @@ def test_numpy_round_trip(batch_size):
     np.testing.assert_allclose(
         R_np, R_recon, rtol=1e-5, atol=1e-6, err_msg=f"NumPy round-trip failed for batch size {batch_size}"
     )
+
+
+@pytest.mark.parametrize("batch_size", [0, 1, 100])
+def test_torch_identity_transform(batch_size):
+    if batch_size == 0:
+        pos = torch.randn(3)
+        b_pos = torch.randn(10, 3)
+    else:
+        pos = torch.randn(batch_size, 3)
+        b_pos = torch.randn(batch_size, 10, 3)
+
+    T_identity = torch.eye(4)
+
+    transformed = transform_by_T(pos, T_identity).cpu().numpy()
+    np.testing.assert_allclose(
+        pos.cpu().numpy(),
+        transformed,
+        rtol=1e-5,
+        atol=1e-6,
+        err_msg=f"NumPy identity_transform failed for batch size {batch_size}",
+    )
+    transformed = transform_by_T(b_pos, T_identity).cpu().numpy()
+    np.testing.assert_allclose(
+        b_pos.cpu().numpy(),
+        transformed,
+        rtol=1e-5,
+        atol=1e-6,
+        err_msg=f"NumPy identity_transform failed for batch size {batch_size}",
+    )
+
+    if batch_size > 0:
+        T_batched_identity = torch.eye(4).reshape(1, 4, 4).repeat(batch_size, 1, 1)
+        transformed = transform_by_T(pos, T_batched_identity).cpu().numpy()
+        np.testing.assert_allclose(
+            pos.cpu().numpy(),
+            transformed,
+            rtol=1e-5,
+            atol=1e-6,
+            err_msg=f"NumPy identity_transform failed for batch size {batch_size}",
+        )
+        transformed = transform_by_T(b_pos, T_batched_identity).cpu().numpy()
+        np.testing.assert_allclose(
+            b_pos.cpu().numpy(),
+            transformed,
+            rtol=1e-5,
+            atol=1e-6,
+            err_msg=f"NumPy identity_transform failed for batch size {batch_size}",
+        )
+
+
+@pytest.mark.parametrize("batch_size", [0, 1, 100])
+def test_numpy_identity_transform(batch_size):
+    if batch_size == 0:
+        pos = np.random.randn(3)
+        b_pos = np.random.randn(10, 3)
+    else:
+        pos = np.random.randn(batch_size, 3)
+        b_pos = np.random.randn(batch_size, 10, 3)
+
+    T_identity = np.eye(4)
+
+    transformed = transform_by_T(pos, T_identity)
+    np.testing.assert_allclose(
+        pos, transformed, rtol=1e-5, atol=1e-6, err_msg=f"NumPy identity_transform failed for batch size {batch_size}"
+    )
+    transformed = transform_by_T(b_pos, T_identity)
+    np.testing.assert_allclose(
+        b_pos, transformed, rtol=1e-5, atol=1e-6, err_msg=f"NumPy identity_transform failed for batch size {batch_size}"
+    )
+
+    if batch_size > 0:
+        T_batched_identity = np.eye(4).reshape(1, 4, 4).repeat(batch_size, 0)
+        transformed = transform_by_T(pos, T_batched_identity)
+        np.testing.assert_allclose(
+            pos,
+            transformed,
+            rtol=1e-5,
+            atol=1e-6,
+            err_msg=f"NumPy identity_transform failed for batch size {batch_size}",
+        )
+        transformed = transform_by_T(b_pos, T_batched_identity)
+        np.testing.assert_allclose(
+            b_pos,
+            transformed,
+            rtol=1e-5,
+            atol=1e-6,
+            err_msg=f"NumPy identity_transform failed for batch size {batch_size}",
+        )
