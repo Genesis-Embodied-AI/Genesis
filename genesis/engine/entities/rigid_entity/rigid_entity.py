be38eb2d591865c70b2278b074d05abf4c8ef5b6
YilingQiao <yilingq97@gmail.com>
2025-06-09 20:29:14 -0700

    only change urdf

--- genesis/engine/entities/rigid_entity/rigid_entity.py
+++ genesis/engine/entities/rigid_entity/rigid_entity.py
@@ -448,21 +448,22 @@ class RigidEntity(Entity):
                 if l_info["parent_idx"] >= 0:
                     l_infos[l_info["parent_idx"]]["is_robot"][()] = True
 
-        for js_gs, js_mj in zip(links_j_infos_gs, links_j_infos):
-            for j_gs, j_mj in zip(js_gs, js_mj):
-                if j_gs["name"] != j_mj["name"]:
-                    gs.logger.warning(f"joint order is not same: {j_gs['name']} != {j_mj['name']}")
-                    continue
-                for k, v in j_gs.items():
-                    if k in j_mj:
-                        if k in ["dofs_force_range", "dofs_armature", "sol_params"]:
-                            # some of mujoco parser's default values are not good for solver
-                            # dofs_force_range: -inf, inf, ours: according to the config file
-                            # dofs_armature: 0.0, ours: 0.1
-                            # dofs_damping: 0.0, ours: 1.0
-                            # dofs_kp: 0.0, ours: 100.0
-                            # dofs_kv: 0.0, ours: 10.0
-                            j_mj[k] = v
+        # Some of mujoco parser's default values are not good for solver. Use our own URDF parser's default values.
+        # dofs_force_range: -inf, inf, ours: according to the config file
+        # dofs_armature: 0.0, ours: 0.1
+        # dofs_damping: 0.0, ours: 1.0
+        # dofs_kp: 0.0, ours: 100.0
+        # dofs_kv: 0.0, ours: 10.0
+        if not isinstance(morph, gs.morphs.MJCF):
+            for js_gs, js_mj in zip(links_j_infos_gs, links_j_infos):
+                for j_gs, j_mj in zip(js_gs, js_mj):
+                    if j_gs["name"] != j_mj["name"]:
+                        gs.logger.warning(f"joint order is not same: {j_gs['name']} != {j_mj['name']}")
+                        continue
+                    for k, v in j_gs.items():
+                        if k in j_mj:
+                            if k in ["dofs_force_range", "dofs_armature", "sol_params"]:
+                                j_mj[k] = v
 
         # Add (link, joints, geoms) tuples sequentially
         for l_info, link_j_infos, link_g_infos in zip(l_infos, links_j_infos, links_g_infos):
