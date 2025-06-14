4e40e61af064fa293a3c06da8bf41d0e5194a939
Zhehuan Chen <50882714+ACMLCZH@users.noreply.github.com>
2025-06-14 00:42:36 -0700

    [Feature] Add USD parsing (#1051)

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -2,12 +2,148 @@ import pytest
 import trimesh
 import numpy as np
 import os
+from huggingface_hub import snapshot_download
 
 import genesis as gs
 import genesis.utils.mesh as mu
 import genesis.utils.gltf as gltf_utils
+import genesis.utils.usda as usda_utils
+
+from .utils import get_hf_assets, assert_allclose, assert_array_equal
+
+VERTICES_TOL = 1e-05  # Transformation loses a little precision in vertices
+NORMALS_TOL = 1e-02  # Conversion from .usd to .glb loses a little precision in normals
 
 
+def check_gs_meshes(gs_mesh1, gs_mesh2, mesh_name):
+    """Check if two gs.Mesh objects are equal."""
+
+    def extract_mesh(gs_mesh):
+        """Extract vertices, normals, uvs, and faces from a gs.Mesh object."""
+        vertices = gs_mesh.trimesh.vertices
+        normals = gs_mesh.trimesh.vertex_normals
+        uvs = gs_mesh.trimesh.visual.uv
+        faces = gs_mesh.trimesh.faces
+
+        indices = np.lexsort(
+            [
+                uvs[:, 1],
+                uvs[:, 0],
+                normals[:, 2],
+                normals[:, 1],
+                normals[:, 0],
+                vertices[:, 2],
+                vertices[:, 1],
+                vertices[:, 0],
+            ]
+        )
+
+        vertices = vertices[indices]
+        normals = normals[indices]
+        uvs = uvs[indices]
+        invdices = np.argsort(indices)
+        faces = invdices[faces]
+        return vertices, faces, normals, uvs
+
+    vertices1, faces1, normals1, uvs1 = extract_mesh(gs_mesh1)
+    vertices2, faces2, normals2, uvs2 = extract_mesh(gs_mesh2)
+
+    assert_allclose(vertices1, vertices2, atol=VERTICES_TOL, err_msg=f"Vertices match failed in mesh {mesh_name}.")
+    assert_array_equal(faces1, faces2, err_msg=f"Faces match failed in mesh {mesh_name}.")
+    assert_allclose(normals1, normals2, atol=NORMALS_TOL, err_msg=f"Normals match failed in mesh {mesh_name}.")
+    assert_allclose(uvs1, uvs2, rtol=gs.EPS, err_msg=f"UVs match failed in mesh {mesh_name}.")
+
+
+def check_gs_tm_meshes(gs_mesh, tm_mesh, mesh_name):
+    """Check if a gs.Mesh object and a trimesh.Trimesh object are equal."""
+    assert_allclose(
+        tm_mesh.vertices,
+        gs_mesh.trimesh.vertices,
+        atol=VERTICES_TOL,
+        err_msg=f"Vertices match failed in mesh {mesh_name}.",
+    )
+    assert_array_equal(
+        tm_mesh.faces,
+        gs_mesh.trimesh.faces,
+        err_msg=f"Faces match failed in mesh {mesh_name}.",
+    )
+    assert_allclose(
+        tm_mesh.vertex_normals,
+        gs_mesh.trimesh.vertex_normals,
+        atol=NORMALS_TOL,
+        err_msg=f"Normals match failed in mesh {mesh_name}.",
+    )
+    if not isinstance(tm_mesh.visual, trimesh.visual.color.ColorVisuals):
+        assert_allclose(
+            tm_mesh.visual.uv,
+            gs_mesh.trimesh.visual.uv,
+            rtol=gs.EPS,
+            err_msg=f"UVs match failed in mesh {mesh_name}.",
+        )
+
+
+def check_gs_tm_textures(gs_texture, tm_color, tm_image, default_value, dim, material_name, texture_name):
+    """Check if a gs.Texture object and a trimesh.Texture object are equal."""
+    if isinstance(gs_texture, gs.textures.ColorTexture):
+        tm_color = tm_color or (default_value,) * dim
+        assert_allclose(
+            tm_color,
+            gs_texture.color,
+            rtol=gs.EPS,
+            err_msg=f"Color mismatch for material {material_name} in {texture_name}.",
+        )
+    elif isinstance(gs_texture, gs.textures.ImageTexture):
+        tm_color = tm_color or (1.0,) * dim
+        assert_allclose(
+            tm_color,
+            gs_texture.image_color,
+            rtol=gs.EPS,
+            err_msg=f"Color mismatch for material {material_name} in {texture_name}.",
+        )
+        assert_array_equal(
+            tm_image,
+            gs_texture.image_array,
+            err_msg=f"Texture mismatch for material {material_name} in {texture_name}.",
+        )
+
+
+def check_gs_textures(gs_texture1, gs_texture2, default_value, material_name, texture_name):
+    """Check if two gs.Texture objects are equal."""
+    if gs_texture1 is None:
+        gs_texture1, gs_texture2 = gs_texture2, gs_texture1
+    if gs_texture1 is not None:
+        gs_texture1 = gs_texture1.check_simplify()
+    if gs_texture2 is not None:
+        gs_texture2 = gs_texture2.check_simplify()
+
+    if isinstance(gs_texture1, gs.textures.ColorTexture):
+        gs_color2 = (default_value,) * len(gs_texture1.color) if gs_texture2 is None else gs_texture2.color
+        assert_allclose(
+            gs_texture1.color,
+            gs_color2,
+            rtol=gs.EPS,
+            err_msg=f"Color mismatch for material {material_name} in {texture_name}.",
+        )
+    elif isinstance(gs_texture1, gs.textures.ImageTexture):
+        assert isinstance(gs_texture2, gs.textures.ImageTexture)
+        assert_allclose(
+            gs_texture1.image_color,
+            gs_texture2.image_color,
+            rtol=gs.EPS,
+            err_msg=f"Color mismatch for material {material_name} in {texture_name}.",
+        )
+        assert_array_equal(
+            gs_texture1.image_array,
+            gs_texture2.image_array,
+            err_msg=f"Texture mismatch for material {material_name} in {texture_name}.",
+        )
+    else:
+        assert (
+            gs_texture1 is None and gs_texture2 is None
+        ), f"Both textures should be None for material {material_name} in {texture_name}."
+
+
+@pytest.mark.required
 @pytest.mark.parametrize("glb_file", ["tests/combined_srt.glb", "tests/combined_transform.glb"])
 def test_glb_parse_geometry(glb_file):
     """Test glb mesh geometry parsing."""
@@ -31,34 +167,10 @@ def test_glb_parse_geometry(glb_file):
     for gs_mesh in gs_meshes:
         mesh_name = gs_mesh.metadata["name"]
         tm_mesh = tm_meshes[mesh_name]
-
-        np.testing.assert_allclose(
-            tm_mesh.vertices,
-            gs_mesh.trimesh.vertices,
-            rtol=0,
-            atol=1e-06,
-            err_msg=f"Vertices match failed in mesh {mesh_name}.",
-        )
-        np.testing.assert_array_equal(
-            tm_mesh.faces,
-            gs_mesh.trimesh.faces,
-            err_msg=f"Faces match failed mesh {mesh_name}.",
-        )
-        np.testing.assert_allclose(
-            tm_mesh.vertex_normals,
-            gs_mesh.trimesh.vertex_normals,
-            rtol=0,
-            atol=1e-06,
-            err_msg=f"Normals match failed mesh {mesh_name}.",
-        )
-        if not isinstance(tm_mesh.visual, trimesh.visual.color.ColorVisuals):
-            np.testing.assert_allclose(
-                tm_mesh.visual.uv,
-                gs_mesh.trimesh.visual.uv,
-                err_msg=f"UVs match failed mesh {mesh_name}.",
-            )
+        check_gs_tm_meshes(gs_mesh, tm_mesh, mesh_name)
 
 
+@pytest.mark.required
 @pytest.mark.parametrize("glb_file", ["tests/chopper.glb"])
 def test_glb_parse_material(glb_file):
     """Test glb mesh geometry parsing."""
@@ -76,36 +188,19 @@ def test_glb_parse_material(glb_file):
         ts_mesh = tm_scene.geometry[geometry_name]
         ts_material = ts_mesh.visual.material.copy()
         tm_materials[ts_material.name] = ts_material
+
     assert len(tm_materials) == len(gs_meshes)
-
-    def check_texture(tm_color, tm_texture, gs_texture, dim, material_name, texture_name):
-        gs_color = gs_texture.color if isinstance(gs_texture, gs.textures.ColorTexture) else gs_texture.image_color
-        tm_color = tm_color or np.ones(dim)
-        np.testing.assert_allclose(
-            tm_color,
-            gs_color,
-            rtol=0,
-            atol=1e-06,
-            err_msg=f"Color mismatch for material {material_name} in {texture_name}.",
-        )
-
-        if tm_texture is not None:
-            np.testing.assert_array_equal(
-                tm_texture,
-                gs_texture.image_array,
-                err_msg=f"Texture mismatch for material {material_name} in {texture_name}.",
-            )
-
     for gs_mesh in gs_meshes:
         material_name = gs_mesh.metadata["name"]
         tm_material = tm_materials[material_name]
         gs_material = gs_mesh.surface
 
         assert isinstance(tm_material, trimesh.visual.material.PBRMaterial)
-        check_texture(
+        check_gs_tm_textures(
+            gs_material.get_texture(),
             tm_material.baseColorFactor,
             np.array(tm_material.baseColorTexture),
-            gs_material.get_texture(),
+            1.0,
             3,
             material_name,
             "color",
@@ -117,18 +212,20 @@ def test_glb_parse_material(glb_file):
             tm_metallic_image = tm_mr_image[:, :, 2]
         else:
             tm_roughness_image, tm_metallic_image = None, None
-        check_texture(
+        check_gs_tm_textures(
+            gs_material.roughness_texture,
             tm_material.roughnessFactor,
             tm_roughness_image,
-            gs_material.roughness_texture,
+            1.0,
             1,
             material_name,
             "roughness",
         )
-        check_texture(
+        check_gs_tm_textures(
+            gs_material.metallic_texture,
             tm_material.metallicFactor,
             tm_metallic_image,
-            gs_material.metallic_texture,
+            0.0,
             1,
             material_name,
             "metallic",
@@ -137,11 +234,61 @@ def test_glb_parse_material(glb_file):
         if tm_material.emissiveFactor is None and tm_material.emissiveFactor is None:
             assert gs_material.emissive_texture is None
         else:
-            check_texture(
+            check_gs_tm_textures(
+                gs_material.emissive_texture,
                 tm_material.emissiveFactor,
                 np.array(tm_material.emissiveTexture),
-                gs_material.emissive_texture,
+                0.0,
                 3,
                 material_name,
                 "emissive",
             )
+
+
+@pytest.mark.required
+@pytest.mark.parametrize("usd_filename", ["usd/sneaker_airforce", "usd/RoughnessTest"])
+def test_usd_parse(usd_filename):
+    asset_path = get_hf_assets(pattern=f"{usd_filename}.glb")
+    glb_file = os.path.join(asset_path, f"{usd_filename}.glb")
+    asset_path = get_hf_assets(pattern=f"{usd_filename}.usdz")
+    usd_file = os.path.join(asset_path, f"{usd_filename}.usdz")
+
+    gs_glb_meshes = gltf_utils.parse_mesh_glb(
+        glb_file,
+        group_by_material=True,
+        scale=1.0,
+        surface=gs.surfaces.Default(),
+    )
+    gs_usd_meshes = usda_utils.parse_mesh_usd(
+        usd_file,
+        group_by_material=True,
+        scale=1.0,
+        surface=gs.surfaces.Default(),
+    )
+
+    assert len(gs_glb_meshes) == len(gs_usd_meshes)
+    gs_glb_mesh_dict = {}
+    for gs_glb_mesh in gs_glb_meshes:
+        gs_glb_mesh_dict[gs_glb_mesh.metadata["name"]] = gs_glb_mesh
+    for gs_usd_mesh in gs_usd_meshes:
+        mesh_name = gs_usd_mesh.metadata["name"].split("/")[-1]
+        gs_glb_mesh = gs_glb_mesh_dict[mesh_name]
+        check_gs_meshes(gs_glb_mesh, gs_usd_mesh, mesh_name)
+
+        gs_glb_material = gs_glb_mesh.surface
+        gs_usd_material = gs_usd_mesh.surface
+        material_name = gs_glb_mesh.metadata["name"]
+        check_gs_textures(gs_glb_material.get_texture(), gs_usd_material.get_texture(), 1.0, material_name, "color")
+        check_gs_textures(
+            gs_glb_material.opacity_texture, gs_usd_material.opacity_texture, 1.0, material_name, "opacity"
+        )
+        check_gs_textures(
+            gs_glb_material.roughness_texture, gs_usd_material.roughness_texture, 1.0, material_name, "roughness"
+        )
+        check_gs_textures(
+            gs_glb_material.metallic_texture, gs_usd_material.metallic_texture, 0.0, material_name, "metallic"
+        )
+        check_gs_textures(gs_glb_material.normal_texture, gs_usd_material.normal_texture, 0.0, material_name, "normal")
+        check_gs_textures(
+            gs_glb_material.emissive_texture, gs_usd_material.emissive_texture, 0.0, material_name, "emissive"
+        )
