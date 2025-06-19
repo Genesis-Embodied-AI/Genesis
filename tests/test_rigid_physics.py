c82fe220b9c08ad0541de65bfb347b87147e207f
Leon Liu <leonjliu2016@gmail.com>
2025-06-19 10:14:29 -0700

    Added unit test to rigid physics and fixed formatting

--- tests/test_rigid_physics.py
+++ tests/test_rigid_physics.py
@@ -2124,6 +2124,33 @@ def test_urdf_mimic(show_viewer, tol):
 
 @pytest.mark.required
 @pytest.mark.parametrize("backend", [gs.cpu])
+def test_drone_hover_same_with_and_without_substeps(show_viewer, tol):
+    base_rpm = 15000
+    scene_ref = gs.Scene(show_viewer=show_viewer, sim_options=gs.options.SimOptions(dt=0.002, substeps=1))
+    drone_ref = scene_ref.add_entity(morph=gs.morphs.Drone(file="urdf/drones/cf2x.urdf", pos=(0, 0, 1.0)))
+    scene_ref.build()
+
+    for _ in range(2500):
+        drone_ref.set_propellels_rpm([base_rpm, base_rpm, base_rpm, base_rpm])
+        scene_ref.step()
+
+    pos_ref = drone_ref.get_dofs_position()
+
+    scene_test = gs.Scene(show_viewer=show_viewer, sim_options=gs.options.SimOptions(dt=0.01, substeps=5))
+    drone_test = scene_test.add_entity(morph=gs.morphs.Drone(file="urdf/drones/cf2x.urdf", pos=(0, 0, 1.0)))
+    scene_test.build()
+
+    for _ in range(500):
+        drone_test.set_propellels_rpm([base_rpm, base_rpm, base_rpm, base_rpm])
+        scene_test.step()
+
+    pos_test = drone_test.get_dofs_position()
+
+    assert_allclose(pos_ref, pos_test, tol=tol)
+
+
+@pytest.mark.required
+@pytest.mark.parametrize("backend", [gs.cpu])
 def test_drone_advanced(show_viewer):
     scene = gs.Scene(
         sim_options=gs.options.SimOptions(
