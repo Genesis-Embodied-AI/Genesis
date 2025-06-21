d7d813dd7672d0f3af6a76db9dd2f898feecf438
Ziheng Liu <53923745+Libero0809@users.noreply.github.com>
2025-06-21 01:01:25 -0700

    [FEATURE] SAP coupler and Hydroelastic contact model. (#1295)
    ---------
    
    Co-authored-by: Alexis DUBURCQ <alexis.duburcq@gmail.com>
    Co-authored-by: YilingQiao <49262224+YilingQiao@users.noreply.github.com>

--- genesis/engine/materials/FEM/base.py
+++ genesis/engine/materials/FEM/base.py
@@ -23,19 +23,32 @@ class Base(Material):
         Poisson ratio, describing the material's volume change under stress. Default is 0.2.
     rho: float, optional
         Material density (kg/m^3). Default is 1000.
+    hydroelastic_modulus: float, optional
+        Hydroelastic modulus for hydroelastic contact. Default is 1e7.
+    friction_mu: float, optional
+        Friction coefficient. Default is 0.1.
+    hessian_invariant: bool, optional
+        If True, Hessian is computed only once. Default is False.
     """
 
     def __init__(
         self,
-        E=1e6,  # Young's modulus
-        nu=0.2,  # Poisson's ratio
-        rho=1000.0,  # density (kg/m^3)
+        E=1e6,
+        nu=0.2,
+        rho=1000.0,
+        hydroelastic_modulus=1e7,
+        friction_mu=0.1,
+        hessian_invariant=False,
     ):
         super().__init__()
 
         self._E = E
         self._nu = nu
         self._rho = rho
+        self._hydroelastic_modulus = hydroelastic_modulus
+        self._friction_mu = friction_mu
+        self.hessian_invariant = hessian_invariant
+        self.hessian_ready = False
 
         # lame parameters: https://github.com/taichi-dev/taichi_elements/blob/d19678869a28b09a32ef415b162e35dc929b792d/engine/mpm_solver.py#L203
         self._mu = E / (2.0 * (1.0 + nu))
@@ -84,3 +97,13 @@ class Base(Material):
     def rho(self):
         """The rest density."""
         return self._rho
+
+    @property
+    def contact_stiffness(self):
+        """The contact stiffness."""
+        return self._hydroelastic_modulus
+
+    @property
+    def friction_mu(self):
+        """The friction coefficient."""
+        return self._friction_mu
