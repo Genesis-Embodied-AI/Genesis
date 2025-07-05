5a8ee3eb7af1f2bce5955489bc0bd697bfc464f1
Trinity Chung <trinityc@andrew.cmu.edu>
2025-07-05 01:01:36 -0400

    add back deleted test

--- tests/test_fem.py
+++ tests/test_fem.py
@@ -293,6 +293,59 @@ def test_sphere_fall_implicit_fem_sap_coupler(fem_material_linear, show_viewer):
         ), f"Entity {entity.uid} minimum Z position {min_pos_z} is not close to 0.0."
 
 
+@pytest.fixture(scope="session")
+def fem_material_linear_corotated():
+    """Fixture for common FEM linear material properties"""
+    return gs.materials.FEM.Elastic(model="linear_corotated")
+
+
+def test_linear_corotated_sphere_fall_implicit_fem_sap_coupler(fem_material_linear_corotated, show_viewer):
+    """Test adding multiple FEM entities to the scene"""
+    scene = gs.Scene(
+        sim_options=gs.options.SimOptions(
+            dt=1.0 / 60.0,
+            substeps=2,
+        ),
+        fem_options=gs.options.FEMOptions(
+            use_implicit_solver=True,
+        ),
+        coupler_options=gs.options.SAPCouplerOptions(),
+        show_viewer=show_viewer,
+        show_FPS=False,
+    )
+
+    scene.add_entity(
+        morph=gs.morphs.Sphere(
+            pos=(0.5, -0.2, 0.5),
+            radius=0.1,
+        ),
+        material=fem_material_linear_corotated,
+    )
+
+    # Build the scene
+    scene.build()
+
+    # Run simulation
+    for _ in range(100):
+        scene.step()
+
+    for entity in scene.entities:
+        state = entity.get_state()
+        pos = tensor_to_array(state.pos.reshape(-1, 3))
+        min_pos_z = np.min(pos[..., 2])
+        assert_allclose(
+            min_pos_z, 0.0, atol=1e-3
+        ), f"Entity {entity.uid} minimum Z position {min_pos_z} is not close to 0.0."
+        BV, BF = igl.bounding_box(pos)
+        x_scale = BV[0, 0] - BV[-1, 0]
+        y_scale = BV[0, 1] - BV[-1, 1]
+        z_scale = BV[0, 2] - BV[-1, 2]
+        assert_allclose(x_scale, 0.2, atol=1e-3), f"Entity {entity.uid} X scale {x_scale} is not close to 0.2."
+        assert_allclose(y_scale, 0.2, atol=1e-3), f"Entity {entity.uid} Y scale {y_scale} is not close to 0.2."
+        # The Z scale is expected to be more squashed due to gravity
+        assert_allclose(z_scale, 0.2, atol=2e-3), f"Entity {entity.uid} Z scale {z_scale} is not close to 0.2."
+
+
 def test_box_hard_vertex_constraint(show_viewer):
     """Test if a box with hard vertex constraints has those vertices fixed,
     and verify updating and removing constraints works correctly."""
