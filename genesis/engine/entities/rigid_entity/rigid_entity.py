changed translation calculation back, used torch tensor, fixed unit test

genesis/engine/entities/rigid_entity/rigid_entity.py
@@ -826,11 +826,10 @@ class RigidEntity(Entity):
         if local_point is None:
             self._kernel_get_jacobian_zero(link.idx)
         else:
-            if not isinstance(local_point, (list, tuple, np.ndarray)):
-                gs.raise_exception("`local_point` must be length-3 iterable.")
-            if len(local_point) != 3:
-                gs.raise_exception("`local_point` must have 3 elements.")
-            p_local = ti.Vector(local_point, dt=ti.f32)
+            p_local = torch.as_tensor(local_point, device=gs.device, dtype=gs.tc_float)
+            if p_local.numel() != 3:
+                gs.raise_exception("`local_point` must have exactly 3 elements.")
+            p_local = ti.Vector([float(p_local[0]), float(p_local[1]), float(p_local[2])])
             self._kernel_get_jacobian(link.idx, p_local)
 
         jacobian = self._jacobian.to_torch(gs.device).permute(2, 0, 1)
@@ -856,9 +855,8 @@ class RigidEntity(Entity):
 
     @ti.kernel
     def _kernel_get_jacobian_zero(self, tgt_link_idx: ti.i32):
-        zero_vec = ti.Vector.zero(ti.f32, 3)
         for i_b in range(self._solver._B):
-            self._impl_get_jacobian(tgt_link_idx, i_b, zero_vec)
+            self._impl_get_jacobian(tgt_link_idx, i_b, ti.Vector.zero(ti.f32, 3))
 
     @ti.func
     def _func_get_jacobian(self, tgt_link_idx, i_b, p_local, pos_mask, rot_mask):
@@ -886,7 +884,7 @@ class RigidEntity(Entity):
                     I_d = [i_d, i_b] if ti.static(self.solver._options.batch_dofs_info) else i_d
                     i_d_jac = i_d + dof_offset - self._dof_start
                     rotation = gu.ti_transform_by_quat(self._solver.dofs_info[I_d].motion_ang, l_state.quat)
-                    translation = (tgt_link_pos - l_state.pos).cross(rotation)
+                    translation = rotation.cross(tgt_link_pos - l_state.pos)
 
                     self._jacobian[0, i_d_jac, i_b] = translation[0] * pos_mask[0]
                     self._jacobian[1, i_d_jac, i_b] = translation[1] * pos_mask[1]
@@ -919,7 +917,7 @@ class RigidEntity(Entity):
                         i_d_jac = i_d + dof_offset - self._dof_start
                         I_d = [i_d, i_b] if ti.static(self.solver._options.batch_dofs_info) else i_d
                         rotation = self._solver.dofs_info[I_d].motion_ang
-                        translation = (tgt_link_pos - l_state.pos).cross(rotation)
+                        translation = rotation.cross(tgt_link_pos - l_state.pos)
 
                         self._jacobian[0, i_d_jac, i_b] = translation[0] * pos_mask[0]
                         self._jacobian[1, i_d_jac, i_b] = translation[1] * pos_mask[1]
@@ -1342,7 +1340,7 @@ class RigidEntity(Entity):
                         # update jacobian for ee link
                         i_l_ee = links_idx[i_ee]
                         self._func_get_jacobian(
-                            i_l_ee, i_b, pos_mask, rot_mask
+                            i_l_ee, i_b, ti.Vector.zero(gs.ti_float, 3), pos_mask, rot_mask
                         )  # NOTE: we still compute jacobian for all dofs as we haven't found a clean way to implement this
 
                         # copy to multi-link jacobian (only for the effective n_dofs instead of self.n_dofs)

tests/test_rigid_physics.py
@@ -2302,34 +2302,29 @@ def test_urdf_mimic(show_viewer, tol):
 @pytest.mark.required
 @pytest.mark.merge_fixed_links(False)
 @pytest.mark.parametrize("model_name", ["pendulum"])
-def test_pendulum_links_acc(tmp_path, gs_sim, show_viewer, tol):
-    urdf_root = _build_multi_pendulum(n=1)
-    urdf_file = tmp_path / "pendulum.urdf"
-    ET.ElementTree(urdf_root).write(urdf_file)
-
-    scene = gs.Scene(sim=gs_sim, show_viewer=show_viewer, show_FPS=False)
-    ent = scene.add_entity(gs.morphs.URDF(file=str(urdf_file), fixed=True))
-    scene.build()
-
+@pytest.mark.parametrize("gs_solver", [gs.constraint_solver.CG])
+@pytest.mark.parametrize("gs_integrator", [gs.integrator.Euler])
+def test_jacobian(gs_sim, tol):
+    pendulum = gs_sim.entities[0]
     angle = 0.7
-    ent.set_qpos(np.array([angle], dtype=np.float32))
-    scene.step()
+    pendulum.set_qpos(np.array([angle], dtype=np.float64))
+    gs_sim.scene.step()
 
-    link = ent.get_link("PendulumArm_0")
+    link = pendulum.get_link("PendulumArm_0")
 
-    p_local = np.array([0.05, -0.02, 0.12], dtype=np.float32)
-    J_o = ent.get_jacobian(link).cpu().numpy()
-    J_p = ent.get_jacobian(link, p_local).cpu().numpy()
+    p_local = np.array([0.05, -0.02, 0.12], dtype=np.float64)
+    J_o = pendulum.get_jacobian(link).cpu().numpy()
+    J_p = pendulum.get_jacobian(link, p_local).cpu().numpy()
 
     c, s = np.cos(angle), np.sin(angle)
-    Rx = np.array([[1, 0, 0], [0, c, -s], [0, s, c]], dtype=np.float32)
+    Rx = np.array([[1, 0, 0], [0, c, -s], [0, s, c]], dtype=np.float64)
     r_world = Rx @ p_local
     r_cross = np.array(
-        [[0, -r_world[2], r_world[1]], [r_world[2], 0, -r_world[0]], [-r_world[1], r_world[0], 0]], dtype=np.float32
+        [[0, -r_world[2], r_world[1]], [r_world[2], 0, -r_world[0]], [-r_world[1], r_world[0], 0]], dtype=np.float64
     )
 
     lin_o, ang_o = J_o[:3, 0], J_o[3:, 0]
-    lin_expected = lin_o + r_cross @ ang_o
+    lin_expected = lin_o - r_cross @ ang_o
 
     assert_allclose(J_p[3:, 0], ang_o, tol=tol)
     assert_allclose(J_p[:3, 0], lin_expected, tol=tol)