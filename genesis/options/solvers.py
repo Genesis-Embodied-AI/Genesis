66b60e90ae5d60528600b213b1dd3044c145cd39
Alexis Duburcq <alexis.duburcq@gmail.com>
2025-07-09 19:05:58 +0200

    Enable GJK by default.

--- genesis/options/solvers.py
+++ genesis/options/solvers.py
@@ -261,8 +261,7 @@ class RigidOptions(Options):
     max_dynamic_constraints : int, optional
         Maximum number of dynamic constraints (like suction cup). Defaults to 8.
     use_gjk_collision: bool, optional
-        Whether to use GJK for collision detection. Defaults to False, because it requires more compilation time
-        than MPR or MPR++. Also, it requires more stress testing before being fully supported.
+        Whether to use GJK for collision detection instead of MPR. Defaults to True.
 
     Warning
     -------
@@ -312,7 +311,7 @@ class RigidOptions(Options):
     enable_mujoco_compatibility: bool = False
 
     # GJK collision detection
-    use_gjk_collision: bool = False
+    use_gjk_collision: bool = True
 
     def __init__(self, **data):
         super().__init__(**data)
