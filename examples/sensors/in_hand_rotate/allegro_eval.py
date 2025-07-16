abc9cd504c1aaafdc2fce631a574d3857d7ba932
Trinity Chung <trinityjchung@gmail.com>
2025-07-15 18:02:27 -0700

    [wip] RL example in hand rotation with tactile sensor

---
+++ examples/sensors/in_hand_rotate/allegro_eval.py
@@ -0,0 +1,63 @@
+import argparse
+import os
+import pickle
+from importlib import metadata
+
+import torch
+
+try:
+    try:
+        if metadata.version("rsl-rl"):
+            raise ImportError
+    except metadata.PackageNotFoundError:
+        if metadata.version("rsl-rl-lib") != "2.2.4":
+            raise ImportError
+except (metadata.PackageNotFoundError, ImportError) as e:
+    raise ImportError("Please uninstall 'rsl_rl' and install 'rsl-rl-lib==2.2.4'.") from e
+from rsl_rl.runners import OnPolicyRunner
+
+import genesis as gs
+
+from allegro_env import AllegroEnv
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--exp_name", type=str, default="allegro-rotation")
+    parser.add_argument("--ckpt", type=int, default=100)
+    args = parser.parse_args()
+
+    gs.init()
+
+    log_dir = f"logs/{args.exp_name}"
+    env_cfg, obs_cfg, reward_cfg, command_cfg, train_cfg = pickle.load(open(f"logs/{args.exp_name}/cfgs.pkl", "rb"))
+    reward_cfg["reward_scales"] = {}
+
+    env = AllegroEnv(
+        num_envs=1,
+        env_cfg=env_cfg,
+        obs_cfg=obs_cfg,
+        reward_cfg=reward_cfg,
+        command_cfg=command_cfg,
+        show_viewer=True,
+    )
+
+    runner = OnPolicyRunner(env, train_cfg, log_dir, device=gs.device)
+    resume_path = os.path.join(log_dir, f"model_{args.ckpt}.pt")
+    runner.load(resume_path)
+    policy = runner.get_inference_policy(device=gs.device)
+
+    obs, _ = env.reset()
+    with torch.no_grad():
+        while True:
+            actions = policy(obs)
+            obs, rews, dones, infos = env.step(actions)
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+# evaluation
+python examples/sensors/in_hand_rotate/allegro_eval.py -e allegro-rotation --ckpt 100
+"""
---
+++ examples/sensors/rigid_tactile_hand.py
@@ -0,0 +1,106 @@
+import argparse
+import genesis as gs
+from genesis.sensors import DataRecordingOptions, RigidContactForceGridSensor
+from genesis.sensors.data_handlers import VideoFileStreamer, NPZFileWriter
+from tqdm import tqdm
+
+import numpy as np
+
+np.set_printoptions(precision=4, suppress=True)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seconds", "-t", type=float, default=1, help="Number of seconds to simulate")
+    parser.add_argument(
+        "--dt",
+        type=float,
+        default=0.01,
+        help="Time step",
+    )
+    parser.add_argument("--n_envs", type=int, default=0, help="Number of environments (default: 0)")
+    parser.add_argument("--cpu", action="store_true", help="Use CPU instead of GPU")
+    parser.add_argument("--vis", "-v", action="store_true", help="Show visualization GUI")
+
+    args = parser.parse_args()
+
+    gs.init(backend=gs.gpu, logging_level=None)
+
+    scene = gs.Scene(
+        sim_options=gs.options.SimOptions(
+            dt=args.dt,
+            gravity=(0, 0, -9.81),
+        ),
+        profiling_options=gs.options.ProfilingOptions(
+            show_FPS=False,
+        ),
+        vis_options=gs.options.VisOptions(
+            background_color=(0.6, 0.6, 0.6),
+        ),
+        show_viewer=args.vis,
+    )
+
+    scene.add_entity(gs.morphs.Plane())
+
+    hand = scene.add_entity(
+        morph=gs.morphs.URDF(
+            file="urdf/allegro_hand/allegro_hand_right_glb.urdf",
+            pos=(0.0, 0.0, 0.1),
+            euler=(0.0, -90.0, 0.0),
+            fixed=True,
+            merge_fixed_links=False,
+        ),
+        material=gs.materials.Rigid(gravity_compensation=1.0),
+    )
+
+    sensors = []
+    for link in hand.links:
+        if "tip" in link.name:
+            print("Adding sensor to link:", link.name)
+            sensor = hand.add_sensor(
+                RigidContactForceGridSensor,
+                name=f"tactile_{link.name}",
+                grid_size=(4, 4, 2),
+                link_idx=link.idx,
+            )
+            sensors.append(sensor)
+    print("Joints:", [joint.name for joint in hand.joints])
+
+    box = scene.add_entity(
+        gs.morphs.Box(pos=(-0.02, 0.0, 0.22), size=(0.09, 0.12, 0.03)),
+        # gs.morphs.Box(pos=(0, 0, 0.15), size=(0.06, 0.09, 0.03)),
+        surface=gs.surfaces.Default(
+            color=(1.0, 0.4, 0.0, 0.5),
+        ),
+    )
+
+    steps = int(args.seconds / args.dt)
+    cam = scene.add_camera(res=(1080, 720), pos=(0.5, 0.7, 0.7), lookat=(0.0, 0.0, 0.1), fov=20, GUI=args.vis)
+
+    # ---- post-build setup ----
+    scene.build(n_envs=args.n_envs)
+
+    dofs_position = [0.1, 0, -0.1, 0.7, 0.6, 0.6, 0.6, 1.0, 0.65, 0.65, 0.65, 1.0, 0.6, 0.6, 0.6, 0.7]
+    if args.n_envs > 0:
+        dofs_position = [dofs_position] * args.n_envs
+    hand.set_dofs_position(np.array(dofs_position))
+
+    cam.start_recording(DataRecordingOptions(handler=VideoFileStreamer(filename="hand_test.mp4", fps=1 / args.dt)))
+    # for sensor in sensors:
+    #     sensor.start_recording(DataRecordingOptions(handler=NPZFileWriter(filename=f"{sensor.name}.npz")))
+
+    # ---- run simulation ----
+    try:
+        for _ in tqdm(range(steps), total=steps):
+            scene.step()
+
+    except KeyboardInterrupt:
+        gs.logger.info("Simulation interrupted, exiting.")
+    finally:
+        gs.logger.info("Simulation finished.")
+
+        scene.stop_recording_all()
+
+
+if __name__ == "__main__":
+    main()
