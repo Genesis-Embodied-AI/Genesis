23b1c908cff73c4497865e1e51f832ed83809554
Alexis DUBURCQ <alexis.duburcq@gmail.com>
2025-07-30 23:52:47 +0200

    [BUG FIX] Various minor fixes. (#1478)
    * More exhaustive testing of rigid data accessors.
    * Add unit test for debug draw.
    * Fix Apple Metal support.
    * Improve numerical robustness of '*_quat_to_xyz' helpers.
    * Fix examples script 'fem_cube_linked_with_arm.py'
    * Fix taichi warning about type casting.
    
    ---------
    
    Co-authored-by: YilingQiao <yilingq97@gmail.com>

--- tests/utils.py
+++ tests/utils.py
@@ -217,17 +217,18 @@ def assert_allclose(actual, desired, *, atol=None, rtol=None, tol=None, err_msg=
     if atol is None:
         atol = 0.0
 
-    if isinstance(actual, torch.Tensor):
-        actual = tensor_to_array(actual)
-    actual = np.asanyarray(actual)
-    if isinstance(desired, torch.Tensor):
-        desired = tensor_to_array(desired)
-    desired = np.asanyarray(desired)
+    args = [actual, desired]
+    for i, arg in enumerate(args):
+        if isinstance(arg, torch.Tensor):
+            arg = tensor_to_array(arg)
+        elif isinstance(arg, (tuple, list)):
+            arg = [tensor_to_array(val) for val in arg]
+        args[i] = np.asanyarray(arg)
 
-    if all(e.size == 0 for e in (actual, desired)):
+    if all(e.size == 0 for e in args):
         return
 
-    np.testing.assert_allclose(actual, desired, atol=atol, rtol=rtol, err_msg=err_msg)
+    np.testing.assert_allclose(*args, atol=atol, rtol=rtol, err_msg=err_msg)
 
 
 def assert_array_equal(actual, desired, *, err_msg=""):
