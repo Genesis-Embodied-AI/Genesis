a03870cde923d815692bbcc6f41a98c455eefeae
Adrian Gasinski <gasnica@gmail.com>
2025-08-04 16:49:34 -0700

    [MISC] Picking boxes and Panda links with a MouseSpring (#1443)

--- genesis/engine/entities/rigid_entity/rigid_link.py
+++ genesis/engine/entities/rigid_entity/rigid_link.py
@@ -15,6 +15,7 @@ from .rigid_geom import RigidGeom, RigidVisGeom
 if TYPE_CHECKING:
     from .rigid_entity import RigidEntity
     from genesis.engine.solvers.rigid.rigid_solver_decomp import RigidSolver
+    from genesis.ext.pyrender.interaction.vec3 import Pose
 
 
 @ti.data_oriented
@@ -591,14 +592,14 @@ class RigidLink(RBC):
         return self._invweight
 
     @property
-    def pos(self):
+    def pos(self) -> ArrayLike:
         """
         The initial position of the link. For real-time position, use `link.get_pos()`.
         """
         return self._pos
 
     @property
-    def quat(self):
+    def quat(self) -> ArrayLike:
         """
         The initial quaternion of the link. For real-time quaternion, use `link.get_quat()`.
         """
@@ -743,6 +744,11 @@ class RigidLink(RBC):
         Whether the entity the link belongs to is free.
         """
         return self.entity.is_free
+
+    @property
+    def pose(self) -> "Pose":
+        """Return the current pose of the link (note, this is not necessarily the same as the principal axes frame)."""
+        return Pose.from_link(self)
 
     # ------------------------------------------------------------------------------------
     # -------------------------------------- repr ----------------------------------------
