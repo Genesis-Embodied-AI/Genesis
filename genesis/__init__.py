c342b0e564f141b76ef80100c0dd36ad34fbbe60
Alexis Duburcq <alexis.duburcq@gmail.com>
2025-08-14 14:27:01 -0700

    Require 'taichi>=1.7.4'.

--- genesis/__init__.py
+++ genesis/__init__.py
@@ -211,9 +211,8 @@ def init(
     with redirect_stdout(_ti_outputs):
         ti.init(
             arch=ti_arch,
-            # Debug mode was buggy and causing segfault prior to v1.7.4
             # Add a (hidden) mechanism to forceable disable taichi debug mode as it is still a bit experimental
-            debug=debug and ti.__version__ >= (1, 7, 4) and (os.environ.get("TI_DEBUG") != "0"),
+            debug=debug and backend == gs.cpu and (os.environ.get("TI_DEBUG") != "0"),
             check_out_of_bound=debug,
             # force_scalarize_matrix=True for speeding up kernel compilation
             # Turning off 'force_scalarize_matrix' is causing numerical instabilities ('nan') on MacOS
