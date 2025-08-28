e75dae1f3b8b41d848faf99b562341fbc75e84c3
Hugh Perkins <hughperkins@gmail.com>
2025-08-28 14:24:36 -0400

    pre-commit

--- tests/test_fastcache_unified.py
+++ tests/test_fastcache_unified.py
@@ -18,7 +18,7 @@ def gs_static_child(args: list[str]):
     parser.add_argument("--test-backend", type=str, choices=["cpu", "gpu"], default="cpu")
     args = parser.parse_args(args)
 
-    print(f'backend {args.test_backend}', type(args.test_backend))
+    print(f"backend {args.test_backend}", type(args.test_backend))
 
     gs.init(backend=getattr(gs, args.test_backend), precision="32")
 
@@ -45,24 +45,37 @@ def gs_static_child(args: list[str]):
     gs.ti.sync()
     print("expected num contacts", args.expected_num_contacts)
     actual_contacts = scene.rigid_solver.collider._collider_state.n_contacts.to_numpy()
-    print('actual', actual_contacts, type(actual_contacts))
+    print("actual", actual_contacts, type(actual_contacts))
     assert scene.rigid_solver.collider._collider_state.n_contacts.to_numpy() == args.expected_num_contacts
     from genesis.engine.solvers.rigid.collider_decomp import func_narrow_phase_convex_vs_convex
-    assert func_narrow_phase_convex_vs_convex._primal.src_ll_cache_observations.cache_key_generated == args.expected_use_src_ll_cache
-    assert func_narrow_phase_convex_vs_convex._primal.src_ll_cache_observations.cache_validated == args.expected_src_ll_cache_hit
-    assert func_narrow_phase_convex_vs_convex._primal.src_ll_cache_observations.cache_loaded == args.expected_src_ll_cache_hit
+
+    assert (
+        func_narrow_phase_convex_vs_convex._primal.src_ll_cache_observations.cache_key_generated
+        == args.expected_use_src_ll_cache
+    )
+    assert (
+        func_narrow_phase_convex_vs_convex._primal.src_ll_cache_observations.cache_validated
+        == args.expected_src_ll_cache_hit
+    )
+    assert (
+        func_narrow_phase_convex_vs_convex._primal.src_ll_cache_observations.cache_loaded
+        == args.expected_src_ll_cache_hit
+    )
 
 
 @pytest.mark.parametrize(
-    "enable_multicontact,expected_num_contacts", [
+    "enable_multicontact,expected_num_contacts",
+    [
         (False, 1),
         (True, 4),
-    ]
+    ],
 )
 @pytest.mark.parametrize("enable_pure", [False, True])  # should not affect result
 # note that using `backend` instead of `test_backend`, breaks genesis pytest...
 @pytest.mark.parametrize("test_backend", ["cpu", "gpu"])  # should not affect result
-def test_gs_static(enable_multicontact: bool, enable_pure: bool, test_backend: str, expected_num_contacts: int, tmp_path: pathlib.Path) -> None:
+def test_gs_static(
+    enable_multicontact: bool, enable_pure: bool, test_backend: str, expected_num_contacts: int, tmp_path: pathlib.Path
+) -> None:
     print("test_gs_static")
     for it in range(3):
         # we iterate to make sure stuff is really being read from cache
@@ -70,10 +83,14 @@ def test_gs_static(enable_multicontact: bool, enable_pure: bool, test_backend: s
             sys.executable,
             __file__,
             gs_static_child.__name__,
-            "--expected-num-contacts", str(expected_num_contacts),
-            "--expected-use-src-ll-cache", "1" if enable_pure else "0",
-            "--expected-src-ll-cache-hit", "1" if enable_pure and it > 0 else "0",
-            "--test-backend", test_backend,
+            "--expected-num-contacts",
+            str(expected_num_contacts),
+            "--expected-use-src-ll-cache",
+            "1" if enable_pure else "0",
+            "--expected-src-ll-cache-hit",
+            "1" if enable_pure and it > 0 else "0",
+            "--test-backend",
+            test_backend,
         ]
         if enable_multicontact:
             cmd_line += ["--enable-multi-contact"]
@@ -86,6 +103,6 @@ def test_gs_static(enable_multicontact: bool, enable_pure: bool, test_backend: s
         assert proc.returncode == 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("__main__")
     globals()[sys.argv[1]](sys.argv[2:])
