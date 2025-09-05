b249434f0dffb39bfeef227f08b852db9f0a80a3
Alexis Duburcq <alexis.duburcq@gmail.com>
2025-09-05 16:57:16 +0200

    Skip tests if fastcache + ndarrays is enabled on apple metal.

--- tests/conftest.py
+++ tests/conftest.py
@@ -208,12 +208,6 @@ def pytest_xdist_auto_num_workers(config):
 
 
 def pytest_runtest_setup(item):
-    # Skip test if gstaichi ndarray mode is enabled but not supported by this specific test
-    if os.environ.get("GS_USE_NDARRAY") == "1":
-        for mark in item.iter_markers("field_only"):
-            if not mark.args or mark.args[0]:
-                pytest.skip(f"This test does not support GsTaichi ndarray mode. Skipping...")
-
     # Enforce GPU affinity if distributed framework is enabled
     worker_id = os.environ.get("PYTEST_XDIST_WORKER")
     if worker_id and worker_id.startswith("gw"):
@@ -381,6 +375,14 @@ def initialize_genesis(request, backend, precision, taichi_offline_cache):
     try:
         if not taichi_offline_cache:
             os.environ["TI_OFFLINE_CACHE"] = "0"
+
+        # Skip test if gstaichi ndarray mode is enabled but not supported by this specific test
+        if os.environ.get("GS_USE_NDARRAY") == "1":
+            for mark in request.node.iter_markers("field_only"):
+                if not mark.args or mark.args[0]:
+                    pytest.skip(f"This test does not support GsTaichi ndarray mode. Skipping...")
+            if os.environ.get("GS_BETA_PURE") == "1" and backend != gs.cpu and sys.platform == "darwin":
+                pytest.skip("fast cache not supported on mac gpus when using ndarray.")
 
         try:
             gs.utils.get_device(backend)
