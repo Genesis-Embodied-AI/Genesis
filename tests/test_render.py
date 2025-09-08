4086db96134480b7229c15a496747738d733c562
YilingQiao <yilingq97@gmail.com>
2025-09-08 11:50:29 -0700

    support rendering deformable body for batched env

--- tests/test_render.py
+++ tests/test_render.py
@@ -857,3 +857,156 @@ def test_render_planes(tmp_path, png_snapshot, renderer):
     for image_file in sorted(tmp_path.rglob("*.png")):
         with open(image_file, "rb") as f:
             assert f.read() == png_snapshot
+
+
+@pytest.mark.required
+@pytest.mark.parametrize("renderer_type", [RENDERER_TYPE.RASTERIZER])
+@pytest.mark.skipif(not IS_INTERACTIVE_VIEWER_AVAILABLE, reason="Interactive viewer not supported on this platform.")
+def test_batch_deformable_render(tmp_path, monkeypatch, png_snapshot, show_viewer):
+    IMAGE_FILENAME = tmp_path / "screenshot_batch_deformable_render.png"
+
+    # Mock 'get_save_filename' to avoid poping up an interactive dialog
+    def get_save_filename(self, file_exts):
+        return IMAGE_FILENAME
+
+    monkeypatch.setattr("genesis.ext.pyrender.viewer.Viewer._get_save_filename", get_save_filename)
+
+    # Mock 'on_key_press' to determine whether requests have been processed
+    is_done = False
+    on_key_press_orig = gs.ext.pyrender.viewer.Viewer.on_key_press
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        nonlocal is_done
+        assert not is_done
+        ret = on_key_press_orig(self, symbol, modifiers)
+        is_done = True
+        return ret
+
+    monkeypatch.setattr("genesis.ext.pyrender.viewer.Viewer.on_key_press", on_key_press)
+
+    scene = gs.Scene(
+        sim_options=gs.options.SimOptions(
+            dt=5e-4,
+            substeps=10,
+        ),
+        pbd_options=gs.options.PBDOptions(
+            particle_size=1e-2,
+        ),
+        mpm_options=gs.options.MPMOptions(
+            lower_bound=(-1.0, -1.0, -0.2),
+            upper_bound=(1.0, 1.0, 1.0),
+        ),
+        sph_options=gs.options.SPHOptions(
+            lower_bound=(-0.5, -0.5, 0.0),
+            upper_bound=(0.5, 0.5, 1),
+            particle_size=0.01,
+        ),
+        viewer_options=gs.options.ViewerOptions(
+            camera_pos=(3.5, 0.0, 2.5),
+            camera_lookat=(0.0, 0.0, 0.5),
+            camera_fov=40,
+        ),
+        vis_options=gs.options.VisOptions(
+            show_world_frame=True,
+            visualize_mpm_boundary=True,
+            visualize_sph_boundary=True,
+        ),
+        show_viewer=True,
+    )
+
+    ########################## entities ##########################
+    frictionless_rigid = gs.materials.Rigid(needs_coup=True, coup_friction=0.0)
+
+    plane = scene.add_entity(
+        material=frictionless_rigid,
+        morph=gs.morphs.Plane(),
+    )
+
+    cube = scene.add_entity(
+        material=frictionless_rigid,
+        morph=gs.morphs.Box(
+            pos=(0.5, 0.5, 0.2),
+            size=(0.2, 0.2, 0.2),
+            euler=(30, 40, 0),
+            fixed=True,
+        ),
+    )
+
+    cloth = scene.add_entity(
+        material=gs.materials.PBD.Cloth(),
+        morph=gs.morphs.Mesh(
+            file="meshes/cloth.obj",
+            scale=1.0,
+            pos=(0.5, 0.5, 0.5),
+            euler=(180.0, 0.0, 0.0),
+        ),
+        surface=gs.surfaces.Default(
+            color=(0.2, 0.4, 0.8, 1.0),
+        ),
+    )
+
+    worm = scene.add_entity(
+        morph=gs.morphs.Mesh(
+            file="meshes/worm/worm.obj",
+            pos=(0.3, 0.3, 0.001),
+            scale=0.1,
+            euler=(90, 0, 0),
+        ),
+        material=gs.materials.MPM.Muscle(
+            E=5e5,
+            nu=0.45,
+            rho=10000.0,
+            model="neohooken",
+            n_groups=4,
+        ),
+    )
+    liquid = scene.add_entity(
+        material=gs.materials.SPH.Liquid(),
+        morph=gs.morphs.Box(
+            pos=(0.0, 0.0, 0.65),
+            size=(0.4, 0.4, 0.4),
+        ),
+        surface=gs.surfaces.Default(
+            color=(0.4, 0.8, 1.0),
+            vis_mode="particle",
+        ),
+    )
+    ########################## build ##########################
+    scene.build(n_envs=4, env_spacing=(2.0, 2.0))
+
+    scene.step()
+
+    pyrender_viewer = scene.visualizer.viewer._pyrender_viewer
+    assert pyrender_viewer.is_active
+
+    # Try saving the current frame
+    pyrender_viewer.dispatch_event("on_key_press", pyglet.window.key.S, 0)
+
+    # Waiting for request completion
+    if pyrender_viewer.run_in_thread:
+        for i in range(100):
+            if is_done:
+                is_done = False
+                break
+            time.sleep(0.1)
+        else:
+            raise AssertionError("Keyboard event not processed before timeout")
+    else:
+        pyrender_viewer.dispatch_pending_events()
+        pyrender_viewer.dispatch_events()
+
+    # test_batch_deformable_render[[]RASTERIZER[]]
+
+    # Skip the rest of the test if necessary
+    if sys.platform == "linux":
+        glinfo = pyrender_viewer.context.get_info()
+        renderer = glinfo.get_renderer()
+        if "llvmpipe" in renderer:
+            llvm_version = re.search(r"LLVM\s+([\d.]+)", renderer).group(1)
+            if llvm_version < "20":
+                pytest.xfail("Text is blurry on Linux using old CPU-based Mesa rendering driver.")
+    # convert to numpy array
+    # Make sure that the result is valid
+    print("IMAGE_FILENAME", IMAGE_FILENAME)
+    with open(IMAGE_FILENAME, "rb") as f:
+        assert f.read() == png_snapshot
