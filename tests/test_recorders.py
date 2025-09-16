c46e5d9957b5fbfc2dad78e627a437eec31b8240
Trinity Chung <trinityjchung@gmail.com>
2025-09-16 18:01:43 -0400

    remove sync timeout

--- tests/test_recorders.py
+++ tests/test_recorders.py
@@ -72,7 +72,7 @@ def test_plotter(png_snapshot, mpl_agg_backend):
         scene.step()
 
     if plotter.run_in_thread:
-        plotter.sync(timeout=10.0)
+        plotter.sync()
     assert call_count == STEPS // 2
     assert len(plotter.x_data) == HISTORY_LENGTH
     assert np.isclose(plotter.x_data[-1], STEPS * DT, atol=gs.EPS)
