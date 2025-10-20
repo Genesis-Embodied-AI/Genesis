8e76d5ad29339303ec2d968cba9dcf00e656ffe0
YilingQiao <yilingq97@gmail.com>
2025-10-19 21:47:31 -0700

    support ndarray for sap coupler

--- examples/sap_coupling/fem_fixed_constraint.py
+++ examples/sap_coupling/fem_fixed_constraint.py
@@ -12,7 +12,7 @@ def main():
     parser.add_argument("-v", "--vis", action="store_true", default=False)
     args = parser.parse_args()
 
-    gs.init(backend=gs.cpu if args.cpu else gs.gpu, precision="64", performance_mode=True)
+    gs.init(backend=gs.cpu if args.cpu else gs.gpu, precision="64")
 
     fem_material_linear_corotated = gs.materials.FEM.Elastic(
         model="linear_corotated",
