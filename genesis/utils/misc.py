7e6d8248ba22cc906632abb32e52a1cb9bf3e88a
Alexis Duburcq <alexis.duburcq@gmail.com>
2025-10-25 15:26:35 +0200

    More robust GPU device management in distributed unit tests.

--- genesis/utils/misc.py
+++ genesis/utils/misc.py
@@ -189,6 +189,8 @@ def get_device(backend: gs_backend, device_idx: Optional[int] = None):
     elif backend == gs_backend.cuda:
         if not torch.cuda.is_available():
             gs.raise_exception("torch cuda not available")
+        if device_idx is None:
+            device_idx = torch.cuda.current_device()
         device = torch.device("cuda", device_idx)
         device_property = torch.cuda.get_device_properties(device)
         device_name = device_property.name
