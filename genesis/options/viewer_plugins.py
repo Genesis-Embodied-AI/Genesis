e2eb9fab8d87369585e5d7d6835132dfea83a831
Trinity Chung <trinityjchung@gmail.com>
2025-11-17 18:44:14 -0500

    add grid snap

--- genesis/options/viewer_plugins.py
+++ genesis/options/viewer_plugins.py
@@ -31,9 +31,13 @@ class MeshPointSelectorPlugin(ViewerPlugin):
         The color of the sphere used to visualize selected points.
     hover_color : tuple
         The color of the sphere used to visualize the point and normal when hovering over a mesh.
+    grid_snap : tuple[float, float, float]
+        Grid snap spacing for each axis (x, y, z). Any negative value disables snapping for that axis.
+        Default is (-1.0, -1.0, -1.0) which means no snapping.
     """
 
     sphere_radius: float = 0.005
-    sphere_color: tuple = (1.0, 0.0, 0.0, 1.0)
-    hover_color: tuple = (1.0, 0.0, 0.0, 1.0)
+    sphere_color: tuple = (0.1, 0.3, 1.0, 1.0)
+    hover_color: tuple = (0.3, 0.5, 1.0, 1.0)
+    grid_snap: tuple[float, float, float] = (-1.0, -1.0, -1.0)
     output_file: str = "selected_points.csv"
