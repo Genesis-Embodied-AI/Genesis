98d3cda59e22be1bcc1068242463212de7db9f1b
Hugh Perkins <hughperkins@gmail.com>
2025-11-25 09:51:39 -0500

    migraet addoption to plugin

--- tests/conftest.py
+++ tests/conftest.py
@@ -359,21 +359,6 @@ def pytest_runtest_setup(item):
             pass
 
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--mem-monitoring-filepath",
-        action="store",
-        default=None,
-        help="Run memory monitoring, and store results to mem_monitoring_filepath. CUDA on linux ONLY.",
-    )
-    parser.addoption("--backend", action="store", default=None, help="Default simulation backend.")
-    parser.addoption(
-        "--logical", action="store_true", default=False, help="Consider logical cores in default number of workers."
-    )
-    parser.addoption("--vis", action="store_true", default=False, help="Enable interactive viewer.")
-    parser.addoption("--dev", action="store_true", default=False, help="Enable genesis debug mode.")
-
-
 @pytest.fixture(scope="session")
 def show_viewer(pytestconfig):
     return pytestconfig.getoption("--vis") and IS_INTERACTIVE_VIEWER_AVAILABLE
