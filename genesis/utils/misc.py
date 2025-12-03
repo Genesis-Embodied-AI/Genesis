ba584aa45bb79a15544406f38f071f01633a62fc
Alexis DUBURCQ <alexis.duburcq@gmail.com>
2025-12-02 23:48:48 -0800

    [BUG FIX] Fix URDF color overwrite. (#2065)
    * Fix URDF color overwrite.
    * Fix zero-copy being disabled for numpy.
    * Add warning when enabling requires_grad while using GsTaichi dynamic arrays.
    * More robust unit tests.

--- genesis/utils/misc.py
+++ genesis/utils/misc.py
@@ -553,11 +553,10 @@ def ti_to_python(
     data_type = type(value)
     use_zerocopy = (
         gs.use_zerocopy
-        and (to_torch or gs.backend == gs.cpu)
         and not issubclass(data_type, ti.Field)
         # and (gs.backend != gs.metal or not issubclass(data_type, ti.Field))
     )
-    if not use_zerocopy:
+    if not use_zerocopy or (not to_torch and gs.backend != gs.cpu):
         if copy is False:
             gs.raise_exception(
                 "Specifying 'copy=False' is not supported if 'gs.use_zerocopy=False' or ('to_torch=False' and "
@@ -570,21 +569,22 @@ def ti_to_python(
     # Leverage zero-copy if enabled
     batch_shape = value.shape
     if use_zerocopy:
-        try:
-            if to_torch or gs.backend != gs.cpu:
-                out = value._T_tc if transpose else value._tc
-            else:
-                out = value._T_np if transpose else value._np
-        except AttributeError:
-            value._tc = torch.utils.dlpack.from_dlpack(value.to_dlpack())
-            value._T_tc = value._tc.movedim(batch_ndim - 1, 0) if (batch_ndim := len(batch_shape)) > 1 else value._tc
-            if to_torch:
-                out = value._T_tc if transpose else value._tc
-            if gs.backend == gs.cpu:
-                value._np = value._tc.numpy()
-                value._T_np = value._T_tc.numpy()
-                if not to_torch:
+        while True:
+            try:
+                if to_torch or gs.backend != gs.cpu:
+                    out = value._T_tc if transpose else value._tc
+                else:
                     out = value._T_np if transpose else value._np
+                break
+            except AttributeError:
+                value._tc = torch.utils.dlpack.from_dlpack(value.to_dlpack())
+                if (batch_ndim := len(batch_shape)) > 1:
+                    value._T_tc = value._tc.movedim(batch_ndim - 1, 0)
+                else:
+                    value._T_tc = value._tc
+                if gs.backend == gs.cpu:
+                    value._np = value._tc.numpy()
+                    value._T_np = value._T_tc.numpy()
         if copy:
             if to_torch:
                 out = out.clone()
