fc37236a88c65189d2d35c9d305942078ca8af04
Hugh Perkins <hughperkins@gmail.com>
2025-12-29 10:27:04 -0500

    make tests importable

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,22 +18,6 @@ from _pytest.mark import Expression, MarkMatcher
 from PIL import Image
 from syrupy.extensions.image import PNGImageSnapshotExtension
 
-
-def pytest_addoption(parser):
-    """Register custom pytest options that need to be available to pytest-xdist workers."""
-    parser.addoption(
-        "--mem-monitoring-filepath",
-        action="store",
-        default=None,
-        help="Run memory monitoring, and store results to mem_monitoring_filepath. CUDA on linux ONLY.",
-    )
-    parser.addoption("--backend", action="store", default=None, help="Default simulation backend.")
-    parser.addoption(
-        "--logical", action="store_true", default=False, help="Consider logical cores in default number of workers."
-    )
-    parser.addoption("--vis", action="store_true", default=False, help="Enable interactive viewer.")
-    parser.addoption("--dev", action="store_true", default=False, help="Enable genesis debug mode.")
-
 has_display = True
 try:
     from tkinter import Tk
