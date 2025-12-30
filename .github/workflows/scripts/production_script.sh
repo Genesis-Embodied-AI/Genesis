55dc8ca1ed48a32a08d9d3263f2c29708b6f6870
Hugh Perkins <hughperkins@gmail.com>
2025-12-30 08:00:09 -0500

    mount cache, rather than venv

---
+++ .github/workflows/scripts/production_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+# sudo apt update
+# sudo apt install -y tmate
+# tmate -S /tmp/tmate.sock new-session -d
+# tmate -S /tmp/tmate.sock wait tmate-ready
+# tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+
+pwd
+ls
+
+uv venv
+source .env/bin/activate
+uv pip install --no-input ".[dev,render]"
+
+pytest --print -x -m "benchmarks" ./tests
+cat speed_test*.txt > "/mnt/data/artifacts/speed_test_${SLURM_JOB_NAME}.txt"
+
+# tmate -S /tmp/tmate.sock wait tmate-exit
