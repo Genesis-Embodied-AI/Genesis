554c9829775a41cced9e92c744dd6e56f5d7a19b
Hugh Perkins <hughperkins@gmail.com>
2025-12-31 16:42:01 -0500

    save current things

--- .github/workflows/scripts/production_build.sh
+++ .github/workflows/scripts/production_build.sh
@@ -14,14 +14,30 @@ whoami
 echo $HOME
 ls ~
 
-if [[ -d /dev/shm/venv ]]; then {
-    mv /dev/shm/venv /dev/shm/_venv
-    rm -Rf /dev/shm/_venv &
-} fi
+VENV_DIR="/venv"
+
+if [[ -d "${VENV_DIR}" ]]; then
+    # Remove existing venv (use unique temp name to avoid collisions)
+    temp_dir="/dev/shm/_venv_$(date +%s)_$$"
+    mv "${VENV_DIR}" "$temp_dir" 2>/dev/null || rm -rf "${VENV_DIR}"
+    rm -rf "$temp_dir" &
+fi
+
+# # Try to create a tmpfs mount inside the container
+# if ! mountpoint -q "${VENV_DIR}"; then
+#     mkdir -m 000 -p "${VENV_DIR}"
+#     if mount -t tmpfs -o size=10G tmpfs "${VENV_DIR}" 2>/dev/null; then
+#         echo "Created tmpfs at ${VENV_DIR}"
+#     else
+#         echo "Warning: Could not create tmpfs, using regular filesystem"
+#     fi
+# fi
+
+df -hT ${VENV_DIR}
 
 python3 -m pip install virtualenv
-virtualenv /dev/shm/venv
-source /dev/shm/venv/bin/activate
+virtualenv "${VENV_DIR}"
+source "${VENV_DIR}/bin/activate"
 # Pre-install problematic packages that conflict with distutils-installed system packages
 pip install --ignore-installed blinker pyparsing setuptools
 pip install ".[dev,render]"
