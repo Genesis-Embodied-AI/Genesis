cf733e692f63c16338c6edac04ae212d10892287
Hugh Perkins <hughperkins@gmail.com>
2026-01-07 08:37:06 -0500

    fuse build scripts

---
+++ .github/workflows/scripts/production_build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -ex
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv --version
+
+# TODO: USD baking does not currently support Python 3.11 since
+# NVIDIA does not currently release `omniverse-kit==107.3` on PyPI.
+# See: https://github.com/Genesis-Embodied-AI/Genesis/pull/1300
+uv venv --python '3.10' /venv
+source /venv/bin/activate
+if [[ "${ADD_OMNI}" == "1" ]]; then
+    uv pip install omniverse-kit --index-url https://pypi.nvidia.com/
+fi
+uv pip install omniverse-kit --index-url https://pypi.nvidia.com/
+# Note: the version of cuda must tightly align with what is being installed
+# in the Slurm container image, otherwise poorly packaged libraries, such as
+# libuipc, may fail to import.
+uv pip install torch --index-url https://download.pytorch.org/whl/cu129
+uv pip install ".[dev,render]"
+if [[ "${ADD_USD}" == "1" ]]; then
+    uv pip install ".[usd]"
+fi
