52728960669c1a2aa95ee1a6c9840804c8ab2927
Trinity Chung <trinityjchung@gmail.com>
2026-01-10 23:55:52 -0500

    add point mesh selector example

--- genesis/ext/pyrender/interaction/plugins/__init__.py
+++ genesis/ext/pyrender/interaction/plugins/__init__.py
@@ -1,6 +1,6 @@
 from .default_controls import DefaultControls
 from .help_text import HelpTextPlugin
-from .mesh_selector import MeshPointSelectorPlugin
+from .mesh_point_selector import MeshPointSelectorPlugin
 from .mouse_spring import MouseSpringPlugin
 
 __all__ = [
