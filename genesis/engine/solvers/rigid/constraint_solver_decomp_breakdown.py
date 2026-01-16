1817fa52e5d6c6920b8a590bb11f27a52e55ed80
Hugh Perkins <hughperkins@gmail.com>
2026-01-16 10:00:40 -0500

    rename kernels

--- genesis/engine/solvers/rigid/constraint_solver_decomp_breakdown.py
+++ genesis/engine/solvers/rigid/constraint_solver_decomp_breakdown.py
@@ -124,14 +124,13 @@ def func_solve_decomposed_microkernels(
 
 
 @ti.kernel(fastcache=gs.use_fastcache)
-def _kernel_step1_linesearch(
+def _kernel_linesearch_top_level(
     entities_info: array_class.EntitiesInfo,
     dofs_state: array_class.DofsState,
     constraint_state: array_class.ConstraintState,
     rigid_global_info: array_class.RigidGlobalInfo,
     static_rigid_sim_config: ti.template(),
 ):
-    """Step 1: Linesearch and update qacc, Ma, Jaref"""
     _B = constraint_state.grad.shape[1]
     ti.loop_config(serialize=static_rigid_sim_config.para_level < gs.PARA_LEVEL.ALL, block_dim=32)
     for i_b in range(_B):
@@ -149,28 +148,26 @@ def _kernel_step1_linesearch(
 
 
 @ti.kernel(fastcache=gs.use_fastcache)
-def _kernel_step2_save_prev_grad(
+def _kernel_cg_only_save_prev_grad(
     constraint_state: array_class.ConstraintState,
     static_rigid_sim_config: ti.template(),
 ):
-    """Step 2: Save prev_grad and prev_Mgrad (CG only)"""
-    if ti.static(static_rigid_sim_config.solver_type == gs.constraint_solver.CG):
-        _B = constraint_state.grad.shape[1]
-        ti.loop_config(serialize=static_rigid_sim_config.para_level < gs.PARA_LEVEL.ALL, block_dim=32)
-        for i_b in range(_B):
-            if constraint_state.n_constraints[i_b] > 0 and constraint_state.improved[i_b]:
-                constraint_solver_decomp.func_save_prev_grad(i_b, constraint_state=constraint_state)
+    """Save prev_grad and prev_Mgrad (CG only)"""
+    _B = constraint_state.grad.shape[1]
+    ti.loop_config(serialize=static_rigid_sim_config.para_level < gs.PARA_LEVEL.ALL, block_dim=32)
+    for i_b in range(_B):
+        if constraint_state.n_constraints[i_b] > 0 and constraint_state.improved[i_b]:
+            constraint_solver_decomp.func_save_prev_grad(i_b, constraint_state=constraint_state)
 
 
 @ti.kernel(fastcache=gs.use_fastcache)
-def _kernel_step3_update_constraint(
+def _kernel_update_constraint(
     entities_info: array_class.EntitiesInfo,
     dofs_state: array_class.DofsState,
     constraint_state: array_class.ConstraintState,
     rigid_global_info: array_class.RigidGlobalInfo,
     static_rigid_sim_config: ti.template(),
 ):
-    """Step 3: Update constraints"""
     _B = constraint_state.grad.shape[1]
     ti.loop_config(serialize=static_rigid_sim_config.para_level < gs.PARA_LEVEL.ALL, block_dim=32)
     for i_b in range(_B):
@@ -187,29 +184,28 @@ def _kernel_step3_update_constraint(
 
 
 @ti.kernel(fastcache=gs.use_fastcache)
-def _kernel_step4_nt_hessian(
+def _kernel_newton_only_nt_hessian_incremental(
     entities_info: array_class.EntitiesInfo,
     constraint_state: array_class.ConstraintState,
     rigid_global_info: array_class.RigidGlobalInfo,
     static_rigid_sim_config: ti.template(),
 ):
     """Step 4: Newton Hessian update (Newton only)"""
-    if ti.static(static_rigid_sim_config.solver_type == gs.constraint_solver.Newton):
-        _B = constraint_state.grad.shape[1]
-        ti.loop_config(serialize=static_rigid_sim_config.para_level < gs.PARA_LEVEL.ALL, block_dim=32)
-        for i_b in range(_B):
-            if constraint_state.n_constraints[i_b] > 0 and constraint_state.improved[i_b]:
-                constraint_solver_decomp.func_nt_hessian_incremental(
-                    i_b,
-                    entities_info=entities_info,
-                    constraint_state=constraint_state,
-                    rigid_global_info=rigid_global_info,
-                    static_rigid_sim_config=static_rigid_sim_config,
-                )
+    _B = constraint_state.grad.shape[1]
+    ti.loop_config(serialize=static_rigid_sim_config.para_level < gs.PARA_LEVEL.ALL, block_dim=32)
+    for i_b in range(_B):
+        if constraint_state.n_constraints[i_b] > 0 and constraint_state.improved[i_b]:
+            constraint_solver_decomp.func_nt_hessian_incremental(
+                i_b,
+                entities_info=entities_info,
+                constraint_state=constraint_state,
+                rigid_global_info=rigid_global_info,
+                static_rigid_sim_config=static_rigid_sim_config,
+            )
 
 
 @ti.kernel(fastcache=gs.use_fastcache)
-def _kernel_step5_update_gradient(
+def _kernel_update_gradient(
     entities_info: array_class.EntitiesInfo,
     dofs_state: array_class.DofsState,
     constraint_state: array_class.ConstraintState,
@@ -232,7 +228,7 @@ def _kernel_step5_update_gradient(
 
 
 @ti.kernel(fastcache=gs.use_fastcache)
-def _kernel_step6_update_search_direction(
+def _kernel_update_search_direction(
     constraint_state: array_class.ConstraintState,
     rigid_global_info: array_class.RigidGlobalInfo,
     static_rigid_sim_config: ti.template(),
@@ -266,38 +262,40 @@ def func_solve_decomposed_macrokernels(
     """
     iterations = rigid_global_info.iterations[None]
     for _it in range(iterations):
-        _kernel_step1_linesearch(
+        _kernel_linesearch_top_level(
             entities_info,
             dofs_state,
             constraint_state,
             rigid_global_info,
             static_rigid_sim_config,
         )
-        _kernel_step2_save_prev_grad(
-            constraint_state,
-            static_rigid_sim_config,
-        )
-        _kernel_step3_update_constraint(
+        if static_rigid_sim_config.solver_type == gs.constraint_solver.CG:
+            _kernel_cg_only_save_prev_grad(
+                constraint_state,
+                static_rigid_sim_config,
+            )
+        _kernel_update_constraint(
             entities_info,
             dofs_state,
             constraint_state,
             rigid_global_info,
             static_rigid_sim_config,
         )
-        _kernel_step4_nt_hessian(
-            entities_info,
-            constraint_state,
-            rigid_global_info,
-            static_rigid_sim_config,
-        )
-        _kernel_step5_update_gradient(
+        if static_rigid_sim_config.solver_type == gs.constraint_solver.Newton:
+            _kernel_newton_only_nt_hessian_incremental(
+                entities_info,
+                constraint_state,
+                rigid_global_info,
+                static_rigid_sim_config,
+            )
+        _kernel_update_gradient(
             entities_info,
             dofs_state,
             constraint_state,
             rigid_global_info,
             static_rigid_sim_config,
         )
-        _kernel_step6_update_search_direction(
+        _kernel_update_search_direction(
             constraint_state,
             rigid_global_info,
             static_rigid_sim_config,
