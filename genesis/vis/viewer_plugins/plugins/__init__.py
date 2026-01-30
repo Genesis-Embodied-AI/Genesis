c7381e20b5d24b715ea92c65fa9600b62ae5db55
Trinity Chung <trinityjchung@gmail.com>
2026-01-30 16:39:05 -0500

    move mesh point selector implementation to examples

--- genesis/vis/viewer_plugins/plugins/__init__.py
+++ genesis/vis/viewer_plugins/plugins/__init__.py
@@ -1,9 +1,7 @@
 from .default_controls import DefaultControlsPlugin
-from .mesh_point_selector import MeshPointSelectorPlugin
 from .mouse_interaction import MouseInteractionPlugin
 
 __all__ = [
     "DefaultControlsPlugin",
-    "MeshPointSelectorPlugin",
     "MouseInteractionPlugin",
 ]
