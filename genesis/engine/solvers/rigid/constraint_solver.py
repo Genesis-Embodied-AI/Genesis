cc37ac10a2be97762141141c6ac2454340a6a714
Mingrui Zhang <mingrui.zh9@gmail.com>
2026-01-31 23:22:25 +0000

    make batched local mem default for debug

--- genesis/engine/solvers/rigid/constraint_solver.py
+++ genesis/engine/solvers/rigid/constraint_solver.py
@@ -28,8 +28,10 @@ USE_DECOMPOSED_MACRO = os.environ.get("GS_SOLVER_DECOMPOSE_MACRO", "0") == "1"
 # USE_DECOMPOSED_MACRO = 1
 # Check environment variable for batched 3-alpha linesearch
 USE_BATCHED_LS = os.environ.get("GS_SOLVER_BATCHED_LS", "0") == "1"
+USE_BATCHED_LS = 1
 # When True, refinement uses local variables instead of global memory for candidates
 USE_BATCHED_LS_LOCAL = os.environ.get("GS_SOLVER_BATCHED_LS_LOCAL", "0") == "1"
+USE_BATCHED_LS_LOCAL = 1
 
 class ConstraintSolver:
     def __init__(self, rigid_solver: "RigidSolver"):
