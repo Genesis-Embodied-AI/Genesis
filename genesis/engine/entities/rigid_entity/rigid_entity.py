6259a10bdfb9d78fcb23479de72884d7ec5cbe17
YilingQiao <yilingq97@gmail.com>
2026-02-02 11:22:54 -0800

    mjcf handling

--- genesis/engine/entities/rigid_entity/rigid_entity.py
+++ genesis/engine/entities/rigid_entity/rigid_entity.py
@@ -3330,12 +3330,9 @@ class RigidEntity(Entity):
         if isinstance(morph, gs.morphs.MJCF):
             if isinstance(morph.file, str):
                 # Try to get model name from MJCF file, fall back to filename stem
-                try:
-                    model_name = mju.get_model_name(morph.file)
-                    if model_name:
-                        return model_name
-                except (ET.ParseError, FileNotFoundError, OSError) as e:
-                    gs.logger.warning(f"Could not extract model name from MJCF: {e}. Using filename stem instead.")
+                model_name = mju.get_model_name(morph.file)
+                if model_name:
+                    return model_name
                 return Path(morph.file).stem
             return morph.file.name
         if isinstance(morph, gs.morphs.Drone):
