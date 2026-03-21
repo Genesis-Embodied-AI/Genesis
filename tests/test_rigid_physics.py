c10286bcf264acd5e22172988a80b0dcd5c9f463
Alexis Duburcq <alexis.duburcq@gmail.com>
2026-03-21 15:35:00 +0100

    Fix color overwrite for MJCF without visuals.

--- tests/test_rigid_physics.py
+++ tests/test_rigid_physics.py
@@ -3098,7 +3098,7 @@ def test_urdf_capsule(tmp_path, show_viewer, tol):
 @pytest.mark.required
 @pytest.mark.required
 @pytest.mark.parametrize("overwrite", [False, True])
-def test_urdf_color_overwrite(overwrite, show_viewer):
+def test_color_overwrite(overwrite, show_viewer):
     scene = gs.Scene(show_viewer=show_viewer)
     box = scene.add_entity(
         gs.morphs.URDF(
@@ -3110,7 +3110,7 @@ def test_urdf_color_overwrite(overwrite, show_viewer):
         ),
     )
     asset_path = get_hf_dataset(pattern="chain.urdf")
-    robot = scene.add_entity(
+    chain = scene.add_entity(
         gs.morphs.URDF(
             file=f"{asset_path}/chain.urdf",
         ),
@@ -3137,6 +3137,15 @@ def test_urdf_color_overwrite(overwrite, show_viewer):
             color=(1.0, 0.0, 0.0, 1.0) if overwrite else None,
         ),
     )
+    asset_path = get_hf_dataset(pattern="humanoid.xml")
+    humanoid = scene.add_entity(
+        gs.morphs.MJCF(
+            file=f"{asset_path}/humanoid.xml",
+        ),
+        surface=gs.surfaces.Default(
+            color=(1.0, 0.0, 0.0, 1.0) if overwrite else None,
+        ),
+    )
     if show_viewer:
         scene.build()
     for vgeom in box.vgeoms:
@@ -3145,12 +3154,28 @@ def test_urdf_color_overwrite(overwrite, show_viewer):
         assert visual.defined
         color = np.unique(visual.vertex_colors, axis=0)
         assert_equal(color, (255, 0, 0, 255) if overwrite else (0, 0, 255, 255))
-    for vgeom in robot.vgeoms:
+    for vgeom in chain.vgeoms:
         assert vgeom.vmesh.metadata["is_visual_overwritten"] == overwrite
         visual = vgeom.vmesh.trimesh.visual
         assert visual.defined
         color = np.unique(visual.vertex_colors, axis=0)
         assert_equal(color, (255, 0, 0, 255) if overwrite else (51, 51, 51, 255))
+    for vgeom in humanoid.vgeoms:
+        # FIXME: The original material is lost because the visuals are collision geometries that has been duplicated as
+        # visual to circumvent the lack of dedicated visuals.
+        is_true_visual = vgeom.vmesh.metadata["name"] == "nose"
+        assert vgeom.vmesh.metadata["is_visual_overwritten"] == overwrite or not is_true_visual
+        visual = vgeom.vmesh.trimesh.visual
+        assert visual.defined
+        color = np.unique(visual.vertex_colors, axis=0)
+        if is_true_visual:
+            if overwrite:
+                assert_equal(color, (255, 0, 0, 255))
+            else:
+                with pytest.raises(AssertionError):
+                    assert_equal(color, (128, 128, 128, 255))
+        else:
+            assert_equal(color, (255, 0, 0, 255) if overwrite else (128, 128, 128, 255))
     for vgeom in axis.vgeoms:
         assert vgeom.vmesh.metadata["is_visual_overwritten"] == overwrite
         visual = vgeom.vmesh.trimesh.visual
