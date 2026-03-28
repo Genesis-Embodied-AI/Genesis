35ca251eab283fcee87e959ab1b887726869812b
Hugh Perkins <hughperkins@gmail.com>
2026-03-28 19:21:01 -0400

    put a comment about picked value

--- genesis/engine/solvers/rigid/constraint/solver.py
+++ genesis/engine/solvers/rigid/constraint/solver.py
@@ -1458,6 +1458,8 @@ def func_hessian_direct_tiled(
     _B = constraint_state.grad.shape[1]
     n_dofs = constraint_state.nt_H.shape[1]
 
+    # BLOCK_DIM = 128 is optimal, after grid searching ofter block_dim = 64, 128, 256, and evaluating
+    # the test_rigid_benchmarks.py in production.yml for each value
     BLOCK_DIM = qd.static(128)
     MAX_DOFS_PER_BLOCK = qd.static(64)
     MAX_CONSTRAINTS_PER_BLOCK = qd.static(32)
