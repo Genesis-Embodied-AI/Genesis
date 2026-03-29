1b4977f5a1199cc856da327317e7a0e9283193fe
Hugh Perkins <hughperkins@gmail.com>
2026-03-29 04:35:43 -0700

    Disable CUDA graph solver when requires_grad is active
    CUDA graph does not support autograd (ndarray args with non-null gradient
    pointers are rejected). Fall back to monolith solver in differentiable mode.

--- genesis/engine/solvers/rigid/constraint/solver_breakdown.py
+++ genesis/engine/solvers/rigid/constraint/solver_breakdown.py
@@ -247,7 +247,7 @@ def _kernel_solve_cuda_graph(
         _func_check_early_exit(constraint_state, graph_counter)
 
 
-@solver.func_solve_body.register(is_compatible=lambda *args, **kwargs: gs.backend in {gs.cuda})
+@solver.func_solve_body.register(is_compatible=lambda entities_info, dofs_state, constraint_state, rigid_global_info, static_rigid_sim_config, _n_iterations: gs.backend in {gs.cuda} and not static_rigid_sim_config.requires_grad)
 def func_solve_decomposed(
     entities_info,
     dofs_state,
