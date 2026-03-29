cb0c5331a7837bb8a0734710d329af095b72eccb
Hugh Perkins <hughperkins@gmail.com>
2026-03-29 16:06:43 -0400

    precommit

--- genesis/engine/solvers/rigid/constraint/solver_breakdown.py
+++ genesis/engine/solvers/rigid/constraint/solver_breakdown.py
@@ -247,7 +247,14 @@ def _kernel_solve_gpu_graph(
         _func_check_early_exit(constraint_state, graph_counter)
 
 
-@solver.func_solve_body.register(is_compatible=lambda entities_info, dofs_state, constraint_state, rigid_global_info, static_rigid_sim_config, _n_iterations: not static_rigid_sim_config.requires_grad)
+@solver.func_solve_body.register(
+    is_compatible=lambda entities_info,
+    dofs_state,
+    constraint_state,
+    rigid_global_info,
+    static_rigid_sim_config,
+    _n_iterations: not static_rigid_sim_config.requires_grad
+)
 def func_solve_decomposed(
     entities_info,
     dofs_state,
