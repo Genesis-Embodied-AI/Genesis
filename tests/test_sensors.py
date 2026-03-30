c4915f588e6361c94eb6021a8d2e68997aad52bd
Mingrui <mingrui.zh9@gmail.com>
2026-03-30 09:40:25 +0100

    force use iterative linesearch for imu test

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -203,6 +203,12 @@ def test_imu_sensor(show_viewer, tol, n_envs):
             substeps=1,
             gravity=(0.0, 0.0, GRAVITY),
         ),
+        # FIXME: Force iterative linesearch: the parallel linesearch's grid search + single Newton
+        # correction accumulates ~1.3e-4 residual over ~60 resting contact steps in float32,
+        # exceeding the 5e-6 tolerance required by the IMU acceleration assertions below.
+        rigid_options=gs.options.RigidOptions(
+            prefer_parallel_linesearch=False,
+        ),
         profiling_options=gs.options.ProfilingOptions(
             show_FPS=False,
         ),
@@ -318,22 +324,19 @@ def test_imu_sensor(show_viewer, tol, n_envs):
     for _ in range(50):
         scene.step()
 
-    # FIXME: Parallel linesearch residual in float32 reaches ~6.5e-5; loosened from 5e-6.
-    assert_allclose(imu.read().lin_acc, (0.0, 0.0, -GRAVITY), tol=7e-5)
+    assert_allclose(imu.read().lin_acc, (0.0, 0.0, -GRAVITY), tol=5e-6)
     assert_allclose(imu.read().ang_vel, (0.0, 0.0, 0.0), tol=1e-5)
     assert_allclose(imu.read().mag, (-MAG_FIELD[0], -MAG_FIELD[1], MAG_FIELD[2]), tol=tol)
 
     # rotate IMU 90 deg around x axis means gravity should be along -y axis
     imu.set_quat_offset(gu.euler_to_quat((90.0, 0.0, 0.0)))
     scene.step()
-    # FIXME: Same parallel linesearch precision issue as above.
-    assert_allclose(imu.read().lin_acc, (0.0, GRAVITY, 0.0), tol=7e-5)
+    assert_allclose(imu.read().lin_acc, (0.0, GRAVITY, 0.0), tol=5e-6)
     assert_allclose(imu.read().mag, (-MAG_FIELD[0], -MAG_FIELD[2], -MAG_FIELD[1]), tol=tol)
 
     imu.set_acc_cross_axis_coupling((0.0, 1.0, 0.0))
     scene.step()
-    # FIXME: Same parallel linesearch precision issue as above.
-    assert_allclose(imu.read().lin_acc, GRAVITY, tol=7e-5)
+    assert_allclose(imu.read().lin_acc, GRAVITY, tol=5e-6)
 
     scene.reset()
     box.set_dofs_velocity((1.0, 2.0, 3.0), dofs_idx_local=slice(3, None))
