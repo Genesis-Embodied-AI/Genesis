9fdd1ff368f1d3912ed2e76ed314aa95c6041741
Mingrui <mingrui.zh9@gmail.com>
2026-03-30 09:31:14 +0100

    add fixme comments

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -318,6 +318,7 @@ def test_imu_sensor(show_viewer, tol, n_envs):
     for _ in range(50):
         scene.step()
 
+    # FIXME: Parallel linesearch residual in float32 reaches ~6.5e-5; loosened from 5e-6.
     assert_allclose(imu.read().lin_acc, (0.0, 0.0, -GRAVITY), tol=7e-5)
     assert_allclose(imu.read().ang_vel, (0.0, 0.0, 0.0), tol=1e-5)
     assert_allclose(imu.read().mag, (-MAG_FIELD[0], -MAG_FIELD[1], MAG_FIELD[2]), tol=tol)
@@ -325,11 +326,13 @@ def test_imu_sensor(show_viewer, tol, n_envs):
     # rotate IMU 90 deg around x axis means gravity should be along -y axis
     imu.set_quat_offset(gu.euler_to_quat((90.0, 0.0, 0.0)))
     scene.step()
+    # FIXME: Same parallel linesearch precision issue as above.
     assert_allclose(imu.read().lin_acc, (0.0, GRAVITY, 0.0), tol=7e-5)
     assert_allclose(imu.read().mag, (-MAG_FIELD[0], -MAG_FIELD[2], -MAG_FIELD[1]), tol=tol)
 
     imu.set_acc_cross_axis_coupling((0.0, 1.0, 0.0))
     scene.step()
+    # FIXME: Same parallel linesearch precision issue as above.
     assert_allclose(imu.read().lin_acc, GRAVITY, tol=7e-5)
 
     scene.reset()
